063c16492b31282840dd76dd1ad200cf7d559aae
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-09 22:04:38 +0100

    Minor comments ( L >> N)

--- PythonApplicationCVX.py
+++ PythonApplicationCVX.py
@@ -7,7 +7,7 @@ def m(X, A):                          # function Y = m(X, A)
     Φ = cos(kron(X, L))               #   Φ = cos(kron(X, 1:L));
     return Φ @ A                      #   Y  = Φ * A;
 
-## Measurements
+## Measurements (note N << L)
 α = randint(-2, 3, (6, 1))            # α = randi([-2, 3], 6, 1);
 ρ = norm(α, 1)
 N  = 128
