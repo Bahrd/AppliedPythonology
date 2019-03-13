8713c301443ddac36de5cca9718d9b68f46f067b
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-13 21:04:53 +0100

    Commitment sync

--- julia.py
+++ julia.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from numpy import linspace as lnsp, power as pwr, mat as mt
 
-# An approximate Julia set function (precision grows with n)
+# An approximate Julia set function (accuracy grows with n)
 # see e.g. https://en.wikipedia.org/wiki/Julia_set for nicer c's
 def julia(x, c = -.835 -.2321j, p = 2, n = 32):
     for _ in range(n):
@@ -10,7 +10,7 @@ def julia(x, c = -.835 -.2321j, p = 2, n = 32):
 
 ## Presentation
 # Unsettling settings
-R = 1; N, M = R * 1024, R * 512
+R = 2; N, M = R * 512, R * 256
 A = mt([[complex(n, m) for n in lnsp(-2, 2, N)] 
                        for m in lnsp(-1, 1, M)])
 # Show off!
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,26 +1,30 @@
 import matplotlib.pyplot as plt
 from numpy import linspace as lnsp, power as pwr, mat as mt
 
-## An approximate Mandelbrot set function 
+## An approximate Mandelbrot set function (accuracy grows with n)
 # see e.g. https://en.wikipedia.org/wiki/Mandelbrot_set
-def mandelbrot(c, n = 32):  # precision grows with n
-    x = complex(0, 0) 
+def mandelbrot(c, n = 32): 
+    x = complex() 
     for _ in range(n):
         x = pwr(x, 2) + c
     return abs(x) < 2       # x ∈ M if |x| < 2
 
 ## Presentation 
 # Rehearsal...
-R = 1; N, M = R * 768, R * 512 
+R = 2; N, M = R * 768, R * 512 
 A = mt([[complex(n, m) for n in lnsp(-2, 1, N)] 
                        for m in lnsp(-1, 1, M)])
 # Act!
 plt.imshow(mandelbrot(A), 
-           #interpolation = 'lanczos', # uncomment if you fancy a colorful image
+           interpolation = 'lanczos', # uncomment if you fancy a colorful image
            cmap = 'Blues')
 plt.show()
 
 ## Outtakes
-#N = 32; M = 48
+#N = 48; M = 32
 #A = mt([[mandelbrot(complex(n, m)) for n in lnsp(-2, 1, N)] 
 #                                   for m in lnsp(-1, 1, M)])
+#plt.imshow(A, 
+#           #interpolation = 'lanczos', # uncomment if you fancy a colorful image
+#           cmap = 'Blues')
+#plt.show()
