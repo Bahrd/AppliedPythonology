a06e2f1bc3e35b8688cff0ff39344ada516edbf9
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-14 14:09:46 +0100

    NaN handling added; Hexadecimals

--- julia.py
+++ julia.py
@@ -1,22 +1,21 @@
-import matplotlib.pyplot as plt
-from numpy import linspace as lnsp, power as pwr, mat as mt, log as nlg
-from math import log10
+import matplotlib.pyplot as plt; from math import log10
+from numpy import linspace as lnsp, power as pwr, mat as mt, isnan
 
 # An approximate Julia set function (accuracy grows with n)
 # see e.g. https://en.wikipedia.org/wiki/Julia_set for nicer c's
-def julia(x, c = -.835 -.2321j, p = 2, n = 32):
+def julia(x, c = -.835 -.2321j, p = 2, n = 0x20):
     for _ in range(n):
         x = pwr(x, p) + c
-    return abs(x) < 2       # x ∈ J if |x| < 2
-    #return abs(x)          # A fancier version
+    #return abs(x) < 2     # A set function (x ∈ J if |x| < 2)
+    return abs(x)          # Not a set function but looks fancier..
 
 ## Presentation
 # Unsettling settings
-R = 16; N, M = R * 64, R * 32
+R = 0x1; N, M = R * 0x40, R * 0x20
 A = mt([[complex(n, m) for n in lnsp(-2, 2, N)] 
                        for m in lnsp(-1, 1, M)])
 # ... and show off!
-J = julia(A)                # J = nlg(julia(A)); J[J > 255] = 255
+J = nlg(julia(A)) #; J[isnan(J)] = 0xff; J[J > 0xff] = 0xff
 plt.imshow(J, 
            #interpolation = 'lanczos', 
            cmap = 'Blues')
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-from numpy import linspace as lnsp, power as pwr, mat as mt
+from numpy import linspace as lnsp, power as pwr, mat as mt, isnan
 
 ## An approximate Mandelbrot set function (accuracy grows with n)
 # see e.g. https://en.wikipedia.org/wiki/Mandelbrot_set
-def mandelbrot(c, n = 64):
+def mandelbrot(c, n = 0x40):
     x = complex() 
     for _ in range(n):
         x = pwr(x, 2) + c
@@ -12,21 +12,12 @@ def mandelbrot(c, n = 64):
 ## Presentation
 # Rehearsal...
 X, Y, eps = -1/2, 0, 1
-R = 16; N, M = R * 32, R * 32 
+R = 0x10; N, M = R * 0x20, R * 0x20 
 A = mt([[complex(n, m) for n in lnsp(X - eps, X + eps, N)] 
                        for m in lnsp(Y - eps, Y + eps, M)])
 # ... and act!
-M = mandelbrot(A)
+M = mandelbrot(A); M[isnan(M)] = 0xff
 plt.imshow(M,
-           interpolation = 'bicubic', # uncomment if you fancy a colorful image
+           #interpolation = 'gaussian', # uncomment if you fancy a colorful image
            cmap = 'Blues')
 plt.show()
-
-## Outtakes
-#N = 48; M = 32
-#A = mt([[mandelbrot(complex(n, m)) for n in lnsp(-2, 1, N)] 
-#                                   for m in lnsp(-1, 1, M)])
-#plt.imshow(A, 
-#           #interpolation = 'lanczos', # uncomment if you fancy a colorful image
-#           cmap = 'Blues')
-#plt.show()
