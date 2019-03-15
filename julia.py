6916cfad98fef19ca9fa46c4b46553496347de5a
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-15 11:21:27 +0100

    Hexadecimation

--- julia.py
+++ julia.py
@@ -1,20 +1,21 @@
 import matplotlib; import matplotlib.pyplot as plt;
 from numpy import linspace as lnsp, power as pwr, mat as mt, isnan
 
-# An approximate Julia set function (accuracy grows with n)
+## An approximate Julia set function (accuracy grows with n)
+#       J = {ω: julia(ω, ∞) < 2}
 # see e.g. https://en.wikipedia.org/wiki/Julia_set for nicer c's
-def julia(x, c = -.8 + .156j, p = 2, n = 0x30):
+def julia(ω, c = -.8 + .156j, p = 2, n = 0x40):
     for _ in range(n):
-        x = pwr(x, p) + c
-    return abs(x)          # Not a set function (x ∈ J if |x| < 2)
-                           # but looks somehow fancier..
+        ω = pwr(ω, p) + c
+    return abs(ω)       # Not exactly a set function
+                        # but looks somehow fancier..
 ## Presentation
 # Unsettling settings
-R = 0x10; N, M = R * 0x40, R * 0x20
-A = mt([[complex(n, m) for n in lnsp(-2, 2, N)] 
+ρ = 0x10; N, M = ρ * 0x40, ρ * 0x20
+Ω = mt([[complex(n, m) for n in lnsp(-2, 2, N)] 
                        for m in lnsp(-1, 1, M)])
-# ... and show off!
-J = julia(A)
+# ... and a show off!
+J = julia(Ω)
 J[isnan(J) | (J > 0xff)] = 0xff
 
 plt.imshow(J, cmap = 'Blues')
