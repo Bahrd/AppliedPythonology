86799a8de57288aee5d3deed1467f14010562a1c
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-16 20:37:09 +0100

    Plain-vanilla

--- julia.py
+++ julia.py
@@ -2,24 +2,20 @@ import matplotlib.pyplot as plt
 from numpy import linspace as lsp, power as pwr, mat as mt, isnan
 
 ## An approximate Julia set function (accuracy grows with ν)
-#       J = {ω: julia(ω, ∞) < 2}
+#       J = {ω ∈ Ω: julia(ω, ∞) < 2}
 # see e.g. https://en.wikipedia.org/wiki/Julia_set for nicer c's
-def julia(ω, c = -.8 + .156j, p = 2, ν = 0x40):
+def julia(ω, ν = 0x40, c = -.8 + .156j, p = 2):
     for _ in range(ν):
         ω = pwr(ω, p) + c
     return abs(ω)           # Not exactly a set function
                             # but looks somehow fancier..
 ## Presentation
-# Unsettling settings (ζ - zoom factor, ρ - resolution)
-ζ, ρ = 0x1, 0x10
-N, M = ρ * 0x40, ρ * 0x20
-n = int(0x40 * pwr(ζ, 1/3)) # Accuracy n should grow with a zoom factor ζ
-                            # A cube root makes it nicer for ζ = 1,...,100
-Ω = mt([[complex(n, m) 
-         for n in lsp(-2/ζ, 2/ζ, N)] 
-         for m in lsp(-1/ζ, 1/ζ, M)])
+# Unsettling settings
+[N, M] = [0x400, 0x200]     # resolution
+Ω = mt([[complex(n, m) for n in lsp(-2, 2, N)] 
+                       for m in lsp(-1, 1, M)])
 # ... and a show off!
-J = julia(Ω, ν = n)
+J = julia(Ω)
 J[isnan(J) | (J > 0xff)] = 0xff # Handling the "NaNs'n'Infs"
 
 plt.imshow(J, cmap = 'Blues')
--- mandelbrot.py
+++ mandelbrot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from numpy import linspace as lnsp, power as pwr, mat as mt, isnan
 
 ## An approximate Mandelbrot set function (accuracy grows with ν)
-#       M = {ω: mandelbrot(ω, ∞) < 2}
+#       M = {ω ∈ Ω: mandelbrot(ω, ∞) < 2}
 # see e.g. https://en.wikipedia.org/wiki/Mandelbrot_set
 def mandelbrot(c, ν = 0x40):
     ω = complex() 
@@ -12,9 +12,8 @@ def mandelbrot(c, ν = 0x40):
 
 ## Presentation
 # Rehearsal...
-X, Y = -1/2, 0 
-ε, ρ = 3/2, 0x10
-N, M = ρ * 0x20, ρ * 0x20 
+[N, M] = [0x200, 0x200]     # resolution
+[X, Y], ε = [-1/2, 0], 3/2  # size
 Ω = mt([[complex(n, m) for n in lnsp(X - ε, X + ε, N)] 
                        for m in lnsp(Y - ε, Y + ε, M)])
 # ... and act!
