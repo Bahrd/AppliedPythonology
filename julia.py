5e0ae4da55074131595bc961a018963e456d33f8
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-16 20:27:40 +0100

    Presentation ready

--- julia.py
+++ julia.py
@@ -1,22 +1,25 @@
-import matplotlib; import matplotlib.pyplot as plt;
-from numpy import linspace as lnsp, power as pwr, mat as mt, isnan
+import matplotlib.pyplot as plt
+from numpy import linspace as lsp, power as pwr, mat as mt, isnan
 
-## An approximate Julia set function (accuracy grows with n)
-#       J = {ω: julia(ω, ∞) < 2}
+## An approximate Julia set function (accuracy grows with ν)
+#       J = {ω ∈ Ω: julia(ω, ∞) < 2}
 # see e.g. https://en.wikipedia.org/wiki/Julia_set for nicer c's
-def julia(ω, c = -.8 + .156j, p = 2, n = 0x40):
-    for _ in range(n):
+def julia(ω, ν = 0x40, c = -.8 + .156j, p = 2):
+    for _ in range(ν):
         ω = pwr(ω, p) + c
-    return abs(ω)       # Not exactly a set function
-                        # but looks somehow fancier..
+    return abs(ω)           # Not exactly a set function
+                            # but looks somehow fancier..
 ## Presentation
-# Unsettling settings
-ρ = 0x10; N, M = ρ * 0x40, ρ * 0x20
-Ω = mt([[complex(n, m) for n in lnsp(-2, 2, N)] 
-                       for m in lnsp(-1, 1, M)])
+# Unsettling settings (ζ - zoom factor, ρ - resolution)
+ζ, ρ = 0x1, 0x10            # size
+N, M = ρ * 0x20, ρ * 0x10   # resolution
+n = int(0x40 * pwr(ζ, 1/3)) # Accuracy set to grow with a zoom factor ζ
+                            # A cube root makes it nicer for ζ = 1,...,100
 # ... and a show off!
-J = julia(Ω)
-J[isnan(J) | (J > 0xff)] = 0xff
+Ω = mt([[complex(n, m) for n in lsp(-2/ζ, 2/ζ, N)] 
+                       for m in lsp(-1/ζ, 1/ζ, M)])
+J = julia(Ω, n)
+J[isnan(J) | (J > 0xff)] = 0xff # Handling the "NaNs'n'Infs"
 
 plt.imshow(J, cmap = 'Blues')
 plt.show()
