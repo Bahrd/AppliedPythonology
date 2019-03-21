dfc33940397968b340e204f74a414d868a710a72
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-21 09:23:39 +0100

    A final BGR to RGB disentangling

--- demosaicking.py
+++ demosaicking.py
@@ -1,28 +1,31 @@
 import cv2; import numpy as np; import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap as lscm
 
 ## Auxiliary functions
 # CFA filter mask
 def CFA(masks, X):
     return np.dstack(np.tile(mask, X) for mask in masks)
 # Channels presentation
-def displayChannels(image, positions, colors, sizeX = 2, sizeY = 2):
+def displayChannels(image, positions, colors, sizeX = 1, sizeY = 4):
     for p, c in zip(positions, colors):
         plt.subplot(sizeX, sizeY, p + 1)
-        plt.imshow(image[..., p], c)
+        cmap = lscm.from_list('mp', ['black', c])
+        plt.imshow(image[..., p], cmap)
     plt.subplot(sizeX, sizeY, sizeX * sizeY)
     plt.imshow(image)
     plt.show()
 
-# Bayer CFA mask segment definition
-BayerMask = np.array([[[0, 1], [0, 0]],             #B
+# Bayer CFA mask segment definition (type 'uint8' required by OpenCV's filters)
+BayerMask = np.array([[[0, 1], [0, 0]],             #R
                       [[1, 0], [0, 1]],             #G
-                      [[0, 0], [1, 0]]], np.uint8)  #R uint8 required by OpenCV
+                      [[0, 0], [1, 0]]], np.uint8)  #B
 # deBayer filter definition
-deBayerMask = np.ones((2, 2)); 
+deBayerMask = np.ones((2, 2))
 
 ## Image 'capturing'
 # Note - the example works for the square images of even size only
-img = cv2.imread("Grasshopper.PNG", cv2.IMREAD_UNCHANGED); N = img.shape[0]; X = [int(N/2), int(N/2)]
+img = cv2.imread("Grasshopper.PNG"); N = img.shape[0]; X = [int(N/2), int(N/2)]
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
 
 ## Mosaicking (simulating the RAW sensor capture)
 # The Bayer color filter array
@@ -34,14 +37,13 @@ img = img * BayerFilter
 # A simple averaging filter
 deBayerFilter = [deBayerMask * w for w in [1, 1/2, 1]]
 # Processing
-B, G, R = [cv2.filter2D(img[..., n], -1, deBayerFilter[n]) for n in range(3)]
+R, G, B = [cv2.filter2D(img[..., n], -1, deBayerFilter[n]) for n in range(3)]
+
 
 ## Presentation
 # A Bayer CFA mosaic (aka 'RAW')
-colors = ["Blues_r", "Greens_r", "Reds_r"] 
+colors = ["red", "green", "blue"] 
 displayChannels(img, range(3), colors)
-
-# A Bayer CFA de-mosaicked image (aka 'JPG')
-bgr = np.dstack((R, G, B))
-colors = ["Reds_r", "Greens_r", "Blues_r"] 
-displayChannels(bgr, range(3), colors)
+# A Bayer CFA de-mosaicked image (aka 'JPG/PNG')
+rgb = np.dstack((R, G, B))
+displayChannels(rgb, range(3), colors)
