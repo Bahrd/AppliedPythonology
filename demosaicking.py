bc702b8a2dafe69400fba138b84028280ee4e50c
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-22 17:13:07 +0100

    Further code suffocation

--- demosaicking.py
+++ demosaicking.py
@@ -26,13 +26,14 @@ deBayerMask = np.ones((2, 2))
 
 ## Image 'capturing'
 # Note - the example works only for square images (N x N, and where N is even)
-img = cv2.imread("GrassHopper.PNG"); N = img.shape[0]; X = [int(N/2), int(N/2)]
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+# and for 2 x 2 filter segments
+img = cv2.imread("GrassHopper.PNG"); img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+N = img.shape[0]; X = [N >> 1]; X *= 2 
 
 ## Mosaicking (capturing an image with a CFA sensor)
 # The Bayer color filter array
 BayerFilter = CFA(BayerMask, X)
-# Processing
+# Filtering the image throught the CFA
 raw = img * BayerFilter
 
 ## Demosaicking 
@@ -41,9 +42,11 @@ deBayerFilter = [deBayerMask * w for w in [1, 1/2, 1]]
 # Evaluating the lacking pixels 
 R, G, B = [cv2.filter2D(raw[..., n], -1, deBayerFilter[n]) for n in range(3)]
 rgb = np.dstack((R, G, B))
-## Restoring the values of raw pixels (optional - demonstrates the difficulty)
+
+## Restoring the values of raw pixels (highlights demosaicking pitfalls)
 #for n in range(3):
 #    rgb[BayerFilter[..., n] != 0, n] = raw[BayerFilter[..., n] != 0, n]
+
 ## Presentation
 # A Bayer CFA mosaic (aka 'RAW')
 colors = ["red", "green", "blue"] 
