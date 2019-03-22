bc3960252808ef8893b2e416b70565c13fc7063a
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-22 23:08:17 +0100

    Fuji X-Trans naive demosaicking

--- demosaicking.py
+++ demosaicking.py
@@ -10,7 +10,7 @@ def CFA(masks, X):
 def displayChannels(image, positions, colors, rows = 1, cols = 4):
     for p, c in zip(positions, colors):
         plt.subplot(rows, cols, p + 1)
-        cmp = lscm.from_list('cmp', ['black', c])
+        cmp = lscm.from_list('_', ['black', c])
         plt.imshow(image[..., p], cmp)
     plt.subplot(rows, cols, rows * cols)
     plt.imshow(image)
@@ -25,28 +25,32 @@ BayerMask = np.array([[[0, 1], [0, 0]],             #R
 deBayerMask = np.ones((2, 2))
 
 ## Image 'capturing'
-# Note - the example works only for square images (N x N, and where N is even)
-img = cv2.imread("GrassHopper.PNG"); N = img.shape[0]; X = [int(N/2), int(N/2)]
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+# Note - the example works only for square images (N x N), for even N, 
+# and for 2 x 2 filter segments
+img = cv2.imread("GrassHopper.PNG"); img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+N = img.shape[0]; X = [N >> 1]; X *= 2 
 
-## Mosaicking (capturing an image with a CFA sensor)
-# The Bayer color filter array
+## Capturing an image with a CFA sensor
+#  Making a CFA
 BayerFilter = CFA(BayerMask, X)
-# Processing
+# Mosaicking, i.e. filtering the image through the CFA
 raw = img * BayerFilter
 
 ## Demosaicking 
-# A simple averaging filter
 deBayerFilter = [deBayerMask * w for w in [1, 1/2, 1]]
-# Processing
+# Evaluating the lacking pixels 
 R, G, B = [cv2.filter2D(raw[..., n], -1, deBayerFilter[n]) for n in range(3)]
+rgb = np.dstack((R, G, B))
+
+## Restoring the values of raw pixels (highlights demosaicking pitfalls)
+#for n in range(3):
+#    rgb[BayerFilter[..., n] != 0, n] = raw[BayerFilter[..., n] != 0, n]
 
 ## Presentation
 # A Bayer CFA mosaic (aka 'RAW')
 colors = ["red", "green", "blue"] 
 displayChannels(raw, range(3), colors)
 # A Bayer CFA de-mosaicked image (aka 'JPG' - I know, I know... )
-rgb = np.dstack((R, G, B))
 displayChannels(rgb, range(3), colors)
 # Input vs. output image
 plt.subplot(131); plt.imshow(img); plt.subplot(132); plt.imshow(rgb)
---
+++ demosaickingX.py
@@ -0,0 +1,72 @@
+import cv2; import numpy as np; import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap as lscm
+
+## Auxiliary functions
+# CFA filter mask (replication of a single CFA segment into a whole sensor mask)
+def CFA(masks, X):
+    return np.dstack(np.tile(mask, X) for mask in masks)
+
+# Image presentation (the channels and the aggregated image - all in a row)
+def displayChannels(image, positions, colors, rows = 1, cols = 4):
+    for p, c in zip(positions, colors):
+        plt.subplot(rows, cols, p + 1)
+        cmp = lscm.from_list('_', ['black', c])
+        plt.imshow(image[..., p], cmp)
+    plt.subplot(rows, cols, rows * cols)
+    plt.imshow(image)
+    plt.show()
+
+## X-Trans(Fuji, 2012)
+# A segment mask definition (type 'uint8' required by OpenCV's filters)
+XTransMask = np.array([[[0, 0, 0, 0, 1, 0], 
+                       [1, 0, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0],
+                       [0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 1],
+                       [0, 1, 0, 0, 0, 0]], 
+                      [[1, 0, 1, 1, 0, 1],
+                       [0, 1, 0, 0, 1, 0],
+                       [1, 0, 1, 1, 0, 1],
+                       [1, 0, 1, 1, 0, 1],
+                       [0, 1, 0, 0, 1, 0],
+                       [1, 0, 1, 1, 0, 1]],
+                      [[0, 1, 0, 0, 0, 0], 
+                       [0, 0, 0, 1, 0, 1],
+                       [0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0],
+                       [1, 0, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0]]], np.uint8)
+# deXTrans filter definition (an example)
+deXTransMask = np.array([[0.  , 0.  , 0.  , 0.  , 0.  , 0.  ],
+                         [0.  , 0.25, 0.5 , 0.5 , 0.25, 0.  ],
+                         [0.  , 0.5 , 1.  , 1.  , 0.5 , 0.  ],
+                         [0.  , 0.5 , 1.  , 1.  , 0.5 , 0.  ],
+                         [0.  , 0.25, 0.5 , 0.5 , 0.25, 0.  ],
+                         [0.  , 0.  , 0.  , 0.  , 0.  , 0.  ]])/9
+## Image 'capturing'
+# Note - the example works only for square images (N x N) with N % 6 = 0, 
+# and for 6 x 6 filter segments
+img = cv2.imread("GrassHopperX.PNG"); img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+N = img.shape[0]; X = [int(N/6)]; X *= 2
+
+## Capturing an image with a CFA sensor
+#  Making a CFA
+XTransFilter = CFA(XTransMask, X)
+# Mosaicking, i.e. filtering the image through the CFA
+raw = img * XTransFilter
+
+## Demosaicking (a very naive approach)
+deXTransFilter = np.array([deXTransMask * w for w in [1/8, 1/20, 1/8]]) * 36
+# Evaluating the lacking pixels 
+R, G, B = [cv2.filter2D(raw[..., n], -1, deXTransFilter[n]) for n in range(3)]
+rgb = np.dstack((R, G, B))
+
+## Presentation
+# An XTrans CFA mosaic (aka 'RAW')
+colors = ["red", "green", "blue"] 
+displayChannels(raw, range(3), colors)
+# An XTrans CFA de-mosaicked image (aka 'JPG' - I know, I know... )
+displayChannels(rgb, range(3), colors)
+# Input vs. output image
+plt.subplot(131); plt.imshow(img); plt.subplot(132); plt.imshow(rgb)
+plt.subplot(133); plt.imshow(img - rgb); plt.show()
