cf316df0a693f672f630c76aee129006adea6bc7
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-03-23 20:42:19 +0100

    A weekend clean up

--- demosaicking.py
+++ demosaicking.py
@@ -2,24 +2,31 @@ import cv2; import numpy as np; import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap as lscm
 
 ## Auxiliary functions
+#Images presentation
+def displayImages(images):
+    for index in range(len(images)):
+        plt.subplot(1, len(images), index + 1)
+        plt.imshow(images[index])
+    plt.show()
+
+# Image dissection presentation (the channels and the aggregated images)
+def displayChannels(images, channels, positions, rows = 1, cols = 4):
+    for image in images:
+        for p, c in zip(positions, channels):
+            plt.subplot(rows, cols, p + 1)
+            cmp = lscm.from_list('_', ['black', c])
+            plt.imshow(image[..., p], cmp)
+        plt.subplot(rows, cols, rows * cols)
+        plt.imshow(image)
+        plt.show()
+
 # CFA filter mask (replication of a single CFA segment into a whole sensor mask)
 def CFA(masks, X):
     return np.dstack(np.tile(mask, X) for mask in masks)
 
-# Image presentation (the channels and the aggregated image - all in a row)
-def displayChannels(image, positions, colors, rows = 1, cols = 4):
-    for p, c in zip(positions, colors):
-        plt.subplot(rows, cols, p + 1)
-        cmp = lscm.from_list('_', ['black', c])
-        plt.imshow(image[..., p], cmp)
-    plt.subplot(rows, cols, rows * cols)
-    plt.imshow(image)
-    plt.show()
-
-
-#### Image 'capturing'
-# Note - the example works only for square images (N x N), for even N, 
-# and for 2 x 2 filter segments
+#### Image 'framing'
+# Note - the example works only for square images (N x N) and for even N 
+# (the filter segments are 2 x 2)
 img = cv2.imread("GrassHopper.PNG"); img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
 N = img.shape[0]; X = [N >> 1]; X *= 2 
 
@@ -32,7 +39,6 @@ BayerMask = np.array([[[0, 1], [0, 0]],             #R
 #  Making a CFA
 BayerFilter = CFA(BayerMask, X)
 
-## Demosaicking 
 # deBayer filter definition (an example)
 deBayerMask = np.ones((2, 2))
 deBayerFilter = [deBayerMask * w for w in [1, 1/2, 1]]
@@ -42,20 +48,15 @@ deBayerFilter = [deBayerMask * w for w in [1, 1/2, 1]]
 #  Mosaicking, i.e. filtering the image through the CFA
 raw = img * BayerFilter
 
+## Demosaicking 
 # Evaluating the lacking pixels 
 R, G, B = [cv2.filter2D(raw[..., n], -1, deBayerFilter[n]) for n in range(3)]
 rgb = np.dstack((R, G, B))
 
-## Restoring the values of raw pixels (highlights demosaicking pitfalls)
-#for n in range(3):
-#    rgb[BayerFilter[..., n] != 0, n] = raw[BayerFilter[..., n] != 0, n]
-
 ## Presentation
-# A Bayer CFA mosaic (aka 'RAW')
-colors = ["red", "green", "blue"] 
-displayChannels(raw, range(3), colors)
-# A Bayer CFA de-mosaicked image (aka 'JPG' - I know, I know... )
-displayChannels(rgb, range(3), colors)
+channels = ("red", "green", "blue")
+# The Bayer CFA mosaic (aka 'RAW') and the de-mosaicked image
+displayChannels((raw, rgb), channels, range(len(channels)))
+
 # Input vs. output image
-plt.subplot(131); plt.imshow(img); plt.subplot(132); plt.imshow(rgb)
-plt.subplot(133); plt.imshow(img - rgb); plt.show()
+displayImages((img, rgb, img - rgb))
--- demosaickingX.py
+++ demosaickingX.py
@@ -2,26 +2,34 @@ import cv2; import numpy as np; import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap as lscm
 
 ## Auxiliary functions
+#Images presentation
+def displayImages(images):
+    for index in range(len(images)):
+        plt.subplot(1, len(images), index + 1)
+        plt.imshow(images[index])
+    plt.show()
+
+# Image dissection presentation (the channels and the aggregated images)
+def displayChannels(images, channels, positions, rows = 1, cols = 4):
+    for image in images:
+        for p, c in zip(positions, channels):
+            plt.subplot(rows, cols, p + 1)
+            cmp = lscm.from_list('_', ['black', c])
+            plt.imshow(image[..., p], cmp)
+        plt.subplot(rows, cols, rows * cols)
+        plt.imshow(image)
+        plt.show()
+
 # CFA filter mask (replication of a single CFA segment into a whole sensor mask)
 def CFA(masks, X):
     return np.dstack(np.tile(mask, X) for mask in masks)
 
-# Image presentation (the channels and the aggregated image - all in a row)
-def displayChannels(image, positions, colors, rows = 1, cols = 4):
-    for p, c in zip(positions, colors):
-        plt.subplot(rows, cols, p + 1)
-        cmp = lscm.from_list('_', ['black', c])
-        plt.imshow(image[..., p], cmp)
-    plt.subplot(rows, cols, rows * cols)
-    plt.imshow(image)
-    plt.show()
-
-
-#### Image 'capturing'
-# Note - the example works only for square images (N x N) with N % 6 = 0, 
-# and for 6 x 6 filter segments
+#### Image 'framing'
+# Note - the example works only for square images (N x N) and 
+# for 6 x 6 filter segments (and hence for N such that N % 6 = 0
 img = cv2.imread("GrassHopperX.PNG"); img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
 N = img.shape[0]; X = [int(N/6)]; X *= 2
+
 
 ## X-Trans (Fuji, 2012)
 # A segment mask definition (type 'uint8' required by OpenCV's filters)
@@ -47,8 +55,8 @@ XTransMask = np.array([[[0, 0, 0, 0, 1, 0],
 XTransFilter = CFA(XTransMask, X)
 
 # An example of the deXTrans filter (far from Fuji... )
-# Note that "secret" weights result from the mosaic and demosaic filters' 
-# shapes: the weighted filter's coefficients (per segment) need to sum-up to 1.
+# Note that the weights' values result from the mosaic and demosaic filters' 
+# shapes: the weighted filters' coefficients (per segment) need to sum-up to 1.
 deXTransMask = np.array([[0.  , 0.  , 0.  , 0.  , 0.  , 0.  ],
                          [0.  , 0.25, 0.5 , 0.5 , 0.25, 0.  ],
                          [0.  , 0.5 , 1.  , 1.  , 0.5 , 0.  ],
@@ -68,10 +76,9 @@ R, G, B = [cv2.filter2D(raw[..., n], -1, deXTransFilter[n]) for n in range(3)]
 rgb = np.dstack((R, G, B))
 
 ## Presentation
+channels = ("red", "green", "blue")
 # An X-Trans CFA mosaic (aka 'RAW') and the de-mosaicked image (aka 'JPG')
-colors = ["red", "green", "blue"] 
-displayChannels(raw, range(3), colors); displayChannels(rgb, range(3), colors)
+displayChannels((raw, rgb), channels, range(len(channels)))
+
 # Input vs. output image
-plt.subplot(131); plt.imshow(img); plt.subplot(132); plt.imshow(rgb)
-plt.subplot(133); plt.imshow(img - rgb)
-plt.show()
+displayImages((img, rgb, img - rgb))
