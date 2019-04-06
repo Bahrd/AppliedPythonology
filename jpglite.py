9d45adbfdd22666d766f94d751f13bb7d071cd0d
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-04-06 22:56:42 +0200

    Fiddling quantization

--- jpglite.py
+++ jpglite.py
@@ -25,9 +25,9 @@ def quantize(X, Q = 1):
         X /= QT; img = np.floor(X + .5); X *= QT
     else:
         X = np.floor(Q*X + .5)/Q
-    return X.astype(np.int)
+    return X
 
-## Image loading (if 'B == 16' then by default a JPG quantization scheme is 
+## Image loading (if 'B == 8' then by default a JPG quantization scheme is 
 #  applied
 img = cv2.cvtColor(cv2.imread("GrassHopper.PNG"), cv2.COLOR_BGR2GRAY); org = img
 N = 512; img, org = cv2.resize(img, (N, N)), cv2.resize(org, (N, N))
@@ -40,7 +40,7 @@ trns = [[dct2(org[n:n + B, m:m + B]) for m in tiles] for n in tiles]
 #  than 'int' conversion) or a standard JPG quatization matrix application)
 #  For instance, 'Q == 0.1' usually results in a poor quality image while
 #  For a JPG, 'Q == 10' yields a visually indistinguishable image
-Q = .1
+Q = 1/4
 ## Coefficients quantization and inverse transformation
 qntz = [[quantize(trns[n][m], Q = Q) for n in blocks] for m in blocks]
 img  = [[   idct2(qntz[n][m])        for n in blocks] for m in blocks]
