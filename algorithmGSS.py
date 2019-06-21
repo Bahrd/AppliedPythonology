c0a7ef1c836f31464ebaf94d27f5a7abff18728e
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2019-06-21 19:16:39 +0200

    Some auxiliary scripts added

---
+++ algorithmGSS.py
@@ -0,0 +1,89 @@
+﻿## The GSS (golden section search algorithm) implementations
+
+# See: https://en.wikipedia.org/wiki/Golden-section_search#Algorithm 
+# Given an interval [l, r] and a maximum of unimodal function f at x, 
+# find an interval [x - ε, x + ε] in the smallest number of steps.
+
+import math
+φ  = (math.sqrt(5) - 1) / 2     # Since 1/φ == φ - 1 
+## Wikipedia's implementation
+def gss(f, a, b, ε = 1e-5, h = None,  c = None, d = None,
+                             fc = None, fd = None):
+    (a, b) = (min(a, b), max(a, b))
+    
+    # Log: print('[{:.4f}, {:.4f}]'.format(a, b))
+    # Interval width
+    if h == None: h = b - a
+    if h <= ε: return (a, b)
+
+    # Interval bounds
+    if c == None: c = a + φ * φ * h
+    if d == None: d = a +  φ * h
+    if fc == None: fc = f(c)
+    if fd == None: fd = f(d)
+    
+    if fc > fd:
+        return gss(f, a, d, ε, h * φ, 
+                      c = None, fc = None, d = c, fd = fc)
+    else:
+        return gss(f, c, b, ε, h * φ, 
+                      c = d, fc = fd, d = None, fd = None)
+
+## Our take:
+# A recursive implementation... 
+def gssl(f, ε, l, r):
+    φ  = (math.sqrt(5) - 1)/2   # Note: φ - 1 == 1/φ
+    n, m = l + (r - l) * φ, r - (r - l) * φ
+    def gsslk(f, ε, l, r, n , m):
+        if r - l <= ε: return (l, r)
+        if f(m) > f(n):
+            r, n = n, m
+            m = r - (r - l) * φ
+            return gsslk(f, ε, l, r, n, m)
+        else:
+            l, m = m, n  
+            n = l + (r - l) * φ
+            return gsslk(f, ε, l, r, n, m)
+    
+    return gsslk(f, ε, l, r, n , m)
+
+# ... wrapped in an OO interface
+class GSS:
+    def __init__(self, f, ε, l, r):
+        self.f, self.ε = f, ε 
+        self.l, self.r = l, r
+        self.φ = (math.sqrt(5) - 1)/2 # Note: φ - 1 == 1/φ
+    
+    def search(self, l, r, n, m):
+        if r - l <= self.ε: return (l, r)
+
+        if self.f(m) > self.f(n):
+            r, n = n, m
+            m = r - (r - l) * φ
+            return self.search(l, r, n, m)
+        else:
+            l, m = m, n  
+            n = l + (r - l) * φ
+            return self.search(l, r, n, m)
+
+    def find(self):
+        l, r, φ = self.l, self.r, self.φ
+        n, m = l + (r - l) * φ, r - (r - l) * φ
+
+        return self.search(self.l, self.r, n, m)
+
+## Tests...
+f = lambda x: -abs(x - 1/φ)
+
+# Wiki's...
+l, r, ε = 0, 2/φ, 1e-3
+(c,d) = gss(f, l, r, ε)
+print('\n[{:.6f}, {:.6f}]'.format(c, d))
+
+# ... and ours
+(c,d) = gssl(f, ε, l, r)
+print('\n[{:.6f}, {:.6f}]'.format(c, d))
+
+gs = GSS(f, ε, l, r)
+(c,d) = gs.find()
+print('\n[{:.6f}, {:.6f}]'.format(c, d))
---
+++ jpg2mpg.py
@@ -0,0 +1,30 @@
+﻿## A JP(E)G to MP4 transcoder 
+# 'C:\Users\Przem\source\repos\Bahrd\images\jpgs'
+#
+# See the source: https://medium.com/@iKhushPatel/convert-video-to-images-images-to-video-using-opencv-python-db27a128a481
+
+import cv2
+from os.path import isfile, join
+from re import search, compile
+
+## Collect JP(E)G filenames
+pathIn, pathOut, ext = '../images/jpgs/', '../images/jpgs/video.mp4', 'jpe?g'
+pattern = re.compile(ext)
+files = [f for f in os.listdir(pathIn) 
+           if isfile(join(pathIn, f)) and pattern.search(f.lower())]
+
+#Sort the filenames (ignoring extension)
+files.sort(key = lambda x: x[0: -len(ext)])
+
+## Compose images into an array
+frame_array = [cv2.imread(join(pathIn, f)) for f in files]
+
+## Setting the video encoder parameters
+fps, fourcc = 4, cv2.VideoWriter_fourcc(*'DIVX')
+# Take a video size from the first frame 
+size = frame_array[0].shape[: 2]          # Ignore the 'layers' parameter             
+out = cv2.VideoWriter(pathOut, fourcc, fps, size[:: -1]) # Swap rows/cols
+# Assemble frames in a video stream
+for f in frame_array:
+    out.write(f)
+out.release()
