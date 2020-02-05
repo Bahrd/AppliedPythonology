50992877e0da62cce3ba4699de24e0a56af5f1b3
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2020-02-05 09:17:18 +0100

    Duda's coding scheme simple implementation

--- ans.py
+++ ans.py
@@ -1,22 +1,23 @@
-﻿from math import floor, ceil;
-x = 1; p = 1/11; y = '1000000000000000000010' # p = P(Y = 1)
+﻿from math import floor, ceil, log2;
+x = 1; p = 1/11; y = '1000000000000000000000001000000000' # p = P(Y = 1)
 
 ## J. Duda 2007 ANS/ABC (see: http://mattmahoney.net/dc/dce.html#Section_33)
 # Encoding
-for i in y:
-    #print(i)
+for i in y[:: -1]:
     if (i == '0'):
         x = ceil((x + 1)/(1 - p)) - 1
     else:
         x = floor(x/p)
-print(x)
 
 # Decoding
+xx, zz = x, ''
 for _ in range(len(y)):
-    z = ceil((x + 1) * p) - ceil(x * p) 
-    #print(z)
+    z = ceil((x + 1) * p) - ceil(x * p); zz += str(z)
     if z == 0:
         x -= ceil(x * p)
     else:
         x = ceil(x * p)
-print(x)
+
+## Presentation
+msg = "Message:{0}\nCode:\t{1}\nBits:\t{2} vs {3}\nDecoded:{4}"
+print(msg.format(y, xx, len(y), floor(log2(xx) + 1), zz))
