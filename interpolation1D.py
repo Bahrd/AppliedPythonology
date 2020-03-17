4a34cc31de8ee5bdfe43b95e13c24b97b9d58d24
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2020-03-17 11:53:32 +0100

    Screamingly interpolated

--- interpolation1D.py
+++ interpolation1D.py
@@ -1,29 +1,12 @@
 import numpy as np; import matplotlib.pyplot as plt
-## Some interpolating functions, Π(x), ψ(x) and ϕ(x) or... 
-#  whatchamacallit this thingamajig...
+from interpolation import Π, ψ, ϕ, interpolate
 
-# The window/rectangular and the hat/tent/triangular (yet anonymous!) function
-Π, ψ =  lambda x, l = -.5, r = .5: (x >= l) * (x < r), lambda x: (1 - abs(x)) * (abs(x) < 1)
-# The Keys' cubic interpolating function (b'cause: https://realpython.com/python-lambda/#syntax)
-def ϕ(x):                       
-    x, a = abs(x), -0.5
-    return (((a + 2) * x**3 - (a + 3) * x**2 + 1)             * Π(x, 0, 1) 
-           + (     a * x**3 -     5*a * x**2 + 8*a * x - 4*a) * Π(x, 0, 2) * (1 - Π(x, 0, 1)))
-
-# An interpolation routine Λ: f(n) ➞ f(x) using ϕ, ψ or Π, 
-def Φ(x, f, λ):
-    n = np.arange(len(f))
-    Λ = λ(x - n)                         
-    return Λ @ f
-
-# An actual interpolation Φ: Fn ➞ Fx 
-def interpolate(fn, N, Λ = [Π, ψ, ϕ]):
-    X = np.linspace(0, len(fn), N)
-    return [[Φ(x, fn, λ) for λ in Λ] for x in X] 
+#Some shortcuts...
+ξ, Λ = interpolate, [ϕ] #ψ, ϕ, Π
 
 ## 1D Examples 
 #  A staple one...
-Fn = [0, 0, 0, 1, 0, 0, 0, 0]; Fx = interpolate(N = 1024, fn = Fn)
+Fn = [0, 0, 0, 1, 0, 0, 0, 0]; Fx = ξ(N = 1024, fn = Fn, Λ = Λ)
 plt.plot(Fx); plt.show()
 #  User-defined...
 while True:
--- interpolation2D.py
+++ interpolation2D.py
@@ -1,27 +1,30 @@
-﻿from auxiliary import Π, ψ, ϕ, interpolate, displayImages
+﻿from interpolation import Π, ψ, ϕ, interpolate
+from auxiliary import displayImages
 import numpy as np; import matplotlib.pyplot as plt
+from random import randrange as RA
 
-## If one wants to go 2D: https://scipython.com/book/chapter-8-scipy/additional-examples/interpolation-of-an-image/
-# In principle, a 2D interpolation, for a separable interpolation function, that is,
-# the function that is a product of 1D interpolation functions, 
+## In principle, a 2D interpolation, for a separable interpolation function, that is,
+#  the function that is a product of 1D interpolation functions, 
 #                      ϕ(x, y) = ϕ(x)ϕ(y)
-# can be implemented as a successive application of the 1D interpolation 
-# procedure to each row and then to each column of the image.
+#  can be implemented as a successive application of the 1D interpolation 
+#  procedure to each row and then to each column of the image.
+## If one wants a serious 2D: https://scipython.com/book/chapter-8-scipy/additional-examples/interpolation-of-an-image/
 
 # Some shortcuts...
-ξ, Λ = interpolate, [ϕ] #ψ, ϕ, Π
+ξ, Λ = interpolate, [ψ] #ψ, ϕ, Π
 # A source image... 
-img = np.array([[0, 0, 0, 1, 1, 0, 0, 0], 
-                [0, 0, 1, 1, 1, 1, 0, 0], 
-                [0, 1, 0, 1, 1, 0, 1, 0], 
-                [1, 1, 0, 1, 1, 0, 1, 1], 
-                [1, 1, 1, 0, 0, 1, 1, 1], 
-                [0, 1, 0, 0, 0, 0, 1, 0], 
-                [0, 0, 1, 1, 1, 1, 0, 0], 
-                [0, 0, 0, 1, 1, 0, 0, 0]])
+s, g = (0x1, 0x0) if RA(2) else (0x0, 0x1)
+img = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 0], 
+                [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+                [0, 1, 0, 0, 1, g, g, 1, 0], 
+                [1, 1, 0, 0, 1, 0, 0, 1, 1], 
+                [1, 1, 1, 1, 0, 1, 1, 1, 1], 
+                [0, 1, s, 1, 1, 1, s, 1, 0], 
+                [0, 0, 1, 0, 0, 0, 1, 0, 0], 
+                [0, 0, 1, g, g, g, 1, 0, 0], 
+                [0, 0, 0, 1, 1, 1, 0, 0, 0]])
 
-M = len(img); N = 17
-
+M = len(img); N = 2*M
 
 # 2D interpolation - simple as that?! (only when M ≤ N...)
 ## A loop-by-loop version
@@ -32,10 +35,3 @@ for n in range(N):
     out[:, n] = np.array(ξ(out[:M, n], N, Λ = Λ)).flat
 
 displayImages((img, out), ('Original', 'Re-scaled'), cmp = 'copper')
-
-## with or without transposition
-#out = np.block([np.array(ξ(img[ m, :], N, Λ = Λ)) for m in range(M)]).T
-#out = np.block([np.array(ξ(out[:M, n], N, Λ = Λ)) for n in range(N)])
-#
-#out = np.block([np.array(ξ(img[m, :], N, Λ = Λ)) for m in range(M)])
-#out = np.block([np.array(ξ(out[n,:M], N, Λ = Λ)) for n in range(N)])
