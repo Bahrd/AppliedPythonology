c75317ca346ce1584352f41c2457781266aaae33
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2020-03-21 11:40:10 +0100

    Round'bout

--- rotationNN.py
+++ rotationNN.py
@@ -1,17 +1,15 @@
 ﻿from auxiliary import displayImages as DI
-from random import randrange, random
+from random import randrange
 from numpy import array, clip, empty
 from math import sin, cos, pi
 from sys import argv
 ## 2D rotation - a canonical version with an implicit NN interpolation
 
 ## Be careful ("Timeo Danaos et dona ferentes!")
-#  Python thinks that 'ϑ == θ' and 'ϱ == ρ'!
+#  Python thinks that 'ϑ is θ == True' (so are 'ϱ' and 'ρ')!
 
-# A shortcut... ;)
-randbin = lambda: randrange(0b10)
 # A source image... 
-s = randbin(); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+s = randrange(0b10); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
                                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
                                          [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0], 
                                          [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0], 
@@ -25,13 +23,14 @@ s = randbin(); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],])
 M = len(img); N = M << 0b11; out = empty((N, N)) 
 
-α = float(argv[1]) if len(argv) == 2 else 360.0 * random() - 180.0 #°
-ϱ = round(α, 1) # an auxiliary variable
-α *= pi/180
+# Setting a rotation angle α
+α = int(argv[1]) if len(argv) == 2 else randrange(-180, 180) #°
+ϱ = α # an auxiliary variable
+α *= pi/180.0
 
-# Rotation of the vector ϑ of coordinates through an angle α w.r.t. OXY
+# Rotation of the vector ϑ of the pixel coordinates through an angle α w.r.t. OXY
 OXY, Rα = array([M/2, M/2]), array([[cos(α), -sin(α)], 
-                                    [sin(α),  cos(α)]]) # clockwise
+                                    [sin(α),  cos(α)]]) # '+' clockwise
 for n in range(N):
     for m in range(N):
         ϑ = array([n/N, m/N]) * M - OXY
