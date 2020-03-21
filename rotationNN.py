b840f5870af706f20a8118bcb04889177dc3a1d5
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2020-03-21 11:01:05 +0100

    Timeo Danaos et dona ferentes...

--- rotationNN.py
+++ rotationNN.py
@@ -1,9 +1,14 @@
-﻿from auxiliary import displayImages
-from random import randrange
+﻿from auxiliary import displayImages as DI
+from random import randrange, random
 from numpy import array, clip, empty
 from math import sin, cos, pi
+from sys import argv
+## 2D rotation - a canonical version with an implicit NN interpolation
 
-# Same shortcuts... ;)
+## Be careful ("Timeo Danaos et dona ferentes!")
+#  Python thinks that 'ϑ == θ' and 'ϱ == ρ'!
+
+# A shortcut... ;)
 randbin = lambda: randrange(0b10)
 # A source image... 
 s = randbin(); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -18,18 +23,19 @@ s = randbin(); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                          [0, 0, 0, 0, 1, g, g, g, 1, 0, 0, 0], 
                                          [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
                                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],])
-M = len(img); N = M << 0b10; out = empty((N, N)) 
+M = len(img); N = M << 0b11; out = empty((N, N)) 
 
-# 2D rotation - a canonical version with an implicit NN interpolation
-α = pi/180 * 31.2 #°
-Rα = array([[cos(α), -sin(α)], [sin(α), cos(α)]]) # clockwise
+α = float(argv[1]) if len(argv) == 2 else 360.0 * random() - 180.0 #°
+ϱ = round(α, 1) # an auxiliary variable
+α *= pi/180
 
 # Rotation of the vector ϑ of coordinates through an angle α w.r.t. OXY
-OXY = array([M/2, M/2])
+OXY, Rα = array([M/2, M/2]), array([[cos(α), -sin(α)], 
+                                    [sin(α),  cos(α)]]) # clockwise
 for n in range(N):
     for m in range(N):
         ϑ = array([n/N, m/N]) * M - OXY
         ϑ = Rα @ ϑ + OXY
         x, y = clip(ϑ, 0, M - 1).astype(int) # where the NNs dwell
         out[n, m] = img[x, y]
-displayImages((img, out), ('Original', 'NN-rotated by {0}°'.format(round(α*180/pi, 1))), cmp = 'copper')
+DI((img, out), ('Original', 'NN-rotated by {0}°'.format(ϱ)), cmp = 'copper')
