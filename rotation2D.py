183d82b6e7b9f2fb5a57f513cd7b9d089ca33b99
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2020-03-22 10:12:16 +0100

    Quick'n'dirty

--- rotation2D.py
+++ rotation2D.py
@@ -9,13 +9,21 @@ from sys import argv
 
 clp = lambda n, nmax, nmin = 0: nmin if n < 0 else n if n < nmax else nmax - 1
 ## Turning a 2D image f(n, m) into a 2D function f(x, y) - using 'Π, ψ, or ϕ'
-def f(x, y, img, λ, Δ = 3):
+def f(x, y, img, λ = ϕ, Δ = 3):
     N, M = img.shape; xx, yy = int(x), int(y)
-    fxy = 0
-    for n in range(clp(xx - Δ, N), clp(xx + Δ, N)):
-        for m in range(clp(yy - Δ, M), clp(yy + Δ, M)):
-            fxy += λ(x - n) * λ(y - m) * img[n, m]
-    return fxy
+    n = np.arange(clp(xx - Δ, N), clp(xx + Δ, N))
+    m = np.arange(clp(yy - Δ, M), clp(yy + Δ, M))
+
+    Λx, Λy = λ(x - n), λ(y - m) 
+    Λxy = np.tensordot(Λx.T, Λy, axes = 0)
+
+    return np.tensordot(Λxy, img[clp(xx - Δ, N):clp(xx + Δ, N), clp(yy - Δ, M):clp(yy + Δ, M)])
+    ## A loop-in-loop version...
+    #fxy = 0.0
+    #for n in range(clp(xx - Δ, N), clp(xx + Δ, N)):
+    #    for m in range(clp(yy - Δ, M), clp(yy + Δ, M)):
+    #        fxy += λ(x - n) * λ(y - m) * img[n, m]
+    #return fxy
 
 # A source image... (cf. './rotationNN.py')
 s = RR(0b10); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -30,7 +38,7 @@ s = RR(0b10); g = s ^ 0b1; img = array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                          [0, 0, 0, 0, 1, g, g, g, 1, 0, 0, 0], 
                                          [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
                                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],])
-M = len(img); N = M << 0b1; out = empty((N, N)) 
+M = len(img); N = M << 0b100; out = empty((N, N)) 
 
 # Setting a rotation angle α
 α = int(argv[1]) if len(argv) == 2 else RR(-180, 180) #°
@@ -44,6 +52,6 @@ for n in range(N):
     for m in range(N):
         ϑ = array([n/N, m/N]) * M - OXY
         x, y = Rα @ ϑ + OXY
-        out[n, m] = f(x, y, img, ϕ)  
+        out[n, m] = f(x, y, img)  
 
 DI((img, out), ('Original', '{0}-rotated by {1}°'.format(ϕ.__name__, ϱ)), cmp = 'copper')
