c0992341adb63a2647ce5531918b7a26c38d89e9
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2020-04-19 21:16:45 +0200

    Randomness, randomness... All is randomness...

--- rotation2D.py
+++ rotation2D.py
@@ -22,8 +22,8 @@ def fl(img, x, y, λ = ϕ, Δ = 3):
         for m in range(*rclp(yy - Δ, yy + Δ, M)):
             fxy += λ(x - n) * λ(y - m) * img[n, m]
     return fxy
-# ... and a quicker'n'cleaner (explicit-loop-free) one
-def f(img, x, y, λ = ϕ, Δ = 3):
+# ... and a quicker'n'cleaner (explicit-loop-free) one 
+def ft(img, x, y, λ = ϕ, Δ = 3):
     N, M = img.shape; xx, yy = int(x), int(y)
     n, m = A(*rclp(xx - Δ, xx + Δ, N)), A(*rclp(yy - Δ, yy + Δ, M))
 
@@ -48,7 +48,7 @@ OXY, Rα = array([M/2, M/2]), array([[cos(α), -sin(α)],
 # ... and an interpoland...:) Π, ψ, ϕ, or ξ, "or else..."
 λλ = argv[3] if len(argv) > 3 else ϕ.__name__; λ = eval(λλ) 
 
-f = f if 0x0 else fl
+f = ft if 0x1 else fl
 if 0b0: 
     # Omloop Het...
     for n in range(N):
@@ -68,3 +68,4 @@ DI((img, out), ('Original', '{0}-rotated by {1}°'.format(λλ, ϱ)), cmp = Cu)
 # 44:   "A imię jego..." A. M. Dz. III 
 # 42:   The Deep Thought's answer ( = 7 * 6)
 # 9/12: A quote of Heidegger/Wittgenstein
+## Note to random fans: f ≠ fl!
