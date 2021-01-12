4d55f97a301bc8ef732ae578129d713037a23ac1
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-01-12 11:05:44 +0100

    Entropy entry...

--- ans.py
+++ ans.py
@@ -1,17 +1,14 @@
 ﻿from math import floor, ceil, log2 as lg2
 from random import randint
 
-# A probability p = P(X = '1') 
-# and the resulting entropy 
-# 'H = -∑pi⋅lg2(pi)' [bit/smbl] 
+# A probability p = P(X = '1') and the resulting entropy 'H = -∑pi⋅lg2(pi)'  
 
-p = 1/64
-H = (p - 1)*lg2(1 - p) - p*lg2(p) 
+p = 1/64; H = (p - 1)*lg2(1 - p) - p*lg2(p) #[bit/smbl]
 
 # A 'random' message...
 prefix, suffix = ('11', '00') if randint(0, 1) else ('00', '11')
-msg = prefix + 0x80 * '0' + suffix
-print(f'Entropy:{H:,.2f} [bit/symbol]\nMessage:{msg}')
+msg = prefix + 0x40 * '0' + suffix
+print(f'P(X = \'1\') = 1/{1/p:,.0f}\nEntropy:{H:,.2f} [bit/symbol]\n\nMessage:{msg}')
 
 ## J. Duda 2007 ANS/ABC (see: http://mattmahoney.net/dc/dce.html#Section_33)
 # Encoding
@@ -28,5 +25,5 @@ for _ in range(len(msg)):
 
 ## Presentation
 ly, lc = len(msg), floor(lg2(code) + 1); rlc = 0o144 * lc/ly
-rprt = 'Encoded:{}\nBits:\t{} vs {} ({:,.2f}%)\nDecoded:{}'
+rprt = 'Encoded:{}\nBits:\t{} vs {} ({:,.0f}%)\n\nDecoded:{}'
 print(rprt.format(code, ly, lc, rlc, dec))
