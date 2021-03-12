b282673ceea26cc4aef986d15b6d01d5504e0a0c
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-03-12 11:10:57 +0100

    Octo++

--- interpolation.py
+++ interpolation.py
@@ -1,9 +1,11 @@
-﻿## Some interpolating functions, Π(x), ψ(x) and ϕ(x) or sinc(x)... 
-#  whatchamacallit this thingamajig...
-# The window/rectangular and the hat/tent/triangular and Keys & sinc (not anonymous!) functions
+﻿''' Some interpolating functions, Π(x), Λ(x) and ϕ(x) or sinc(x) for that matter...
+
+Whatchamacallit this thingamajig: 
+* The window/rectangular and the hat/tent/triangular and Keys & sinc (not anonymous!) functions. 
+* The Keys' cubic interpolating function (b'cause: https://realpython.com/python-lambda/#syntax)'''
+
 def Π(x, l = -.5, r = .5): return (x >= l) * (x < r)
 def Λ(x): return (1 - abs(x)) * (abs(x) < 1)
-# The Keys' cubic interpolating function (b'cause: https://realpython.com/python-lambda/#syntax)
 def ϕ(x): 
     x, a = abs(x), -.5
     return (((a + 2) * x**3 - (a + 3) * x**2 + 1)             * Π(x, 0, 1) 
@@ -13,7 +15,7 @@ from numpy import sin, ones_like, pi as π
 def ξ(x, m = π): 
     x = x * m; sinc = ones_like(x)
     sinc[x != 0] = sin(x[x != 0])/(x[x != 0])
-    return sinc
+    return sinc # sinc for syncope?
 # Or, simply...
 #from numpy import sinc
 
