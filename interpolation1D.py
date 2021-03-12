bab89528b1dd5a39d6d516ba92b71d5cc2f35e48
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-03-12 10:52:47 +0100

    Octoplus...

--- interpolation1D.py
+++ interpolation1D.py
@@ -3,15 +3,15 @@ from interpolation import Π, Λ, ϕ, ξ, interpolate
 from auxiliary import displayPlots
 
 #Some shortcuts...
-Σφ, κ = interpolate, 'Π, Λ, ϕ, ξ'
-φ, φφ = list(eval(κ)), κ.split(',')  
+Σψ, κ = interpolate, 'Π, Λ, ϕ, ξ'
+ψ, ψψ = list(eval(κ)), κ.split(',')  
 
 ## 1D Examples 
 #  A staple one...
 Fn = [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-Fx = np.array(Σφ(N = 1024, fn = Fn, φ = φ)).T
+Fx = np.array(Σψ(N = 0o2000, fn = Fn, φ = ψ)).T         # Octoplus!
 
-displayPlots(Fx.T, φφ); plt.plot(Fx); plt.title(κ); plt.show()
+displayPlots(Fx.T, ψψ); plt.plot(Fx); plt.title(κ); plt.show()
 
 #  User-defined...
 while True:
@@ -19,6 +19,6 @@ while True:
     if len(rawFn) > 1:
         N = int(input("Output size (N): ")); Fn = [float(n) for n in rawFn]        
         # 2in1: computations and presentation 
-        displayPlots(Σφ(Fn, N), φφ)        
+        displayPlots(Σψ(Fn, N), ψψ)        
     else:
         break
