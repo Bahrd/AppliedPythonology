b157a1031651b72191e9816e7ea3add03c837e22
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-04-11 09:58:18 +0200

    Quantum leap II

--- AppliedMathology/VanilaQS.m
+++ AppliedMathology/VanilaQS.m
@@ -1,7 +1,7 @@
 %% QuQuRickU
 %% A simple implementation of a basic Grover's quantum search algorithm
 % Number of qubits
-Q = 5; NoS = 2^Q; 
+Q = 3; NoS = 2^Q; 
 % A location n of a marked state is randomly selected here
 n = randi(NoS);
 % A shortcut to a flipper
@@ -9,24 +9,27 @@ R = @(n) fR(n, NoS);
 %Hadamard matrix
 H = hadamard(NoS)/sqrt(NoS);
 %Grover operator matrix
-G = -R(n)*H*R(1)*H;
+G = -R(n) * H * R(1) * H;
 
-%% Uno, due, tre...
+%% Uno, due, tre... 
 phi_0 = zeros(NoS, 1); phi_0(1) = 1;
-phi = R(n)*H*phi_0;                 % Initial step (inverting a phase of the marked state)
-for i = 1:floor(pi*sqrt(NoS)/4)
-    phi = G*phi;                    % Step-by-step amplitude amplification 
-    sprintf('%dth iteration yields %d with prob. %0.3g', i, n, phi(n)^2)
-end
-% or in an 'en bloc!' version...
-phi = R(n)*H*phi_0;
+% NUmber of Grover operator iteration
 q = floor(pi*sqrt(NoS)/4);
-phi = (G^q)*phi;              
-sprintf('%dth power of G yields %d with prob. %0.3g', q, n, phi(n)^2)
+
+% An iterative (more 'quantum implementable') version... 
+% (forgive the lack of the 1st/2n/third distinction please! ;)
+phi = R(n) * H * phi_0;                   % Initial step (inverting a phase of the marked state)
+for i = 1:q
+    phi = G * phi;                        % Step-by-step amplitude amplification 
+    sprintf('After %dth iteration P(No = %d) = %0.3g', i, n, phi(n)^2)
+end
+% or its 'en bloc genre'...
+phi = (G^q) * R(n) * H * phi_0;       
+sprintf('P(No = %d) = %0.3g', n, phi(n)^2)
 
 %% Flipper (aka 'amplitude sign flipping operator')
 % Flipping the sign of the ampitude of the state |n> operator
-% (which happens to be a mean after Walsh-Hadamard transform when |0>)
+% (which happens to be a mean at |0> after Walsh-Hadamard transform)
 function r = fR(n, NoS)
     psi = zeros(NoS, 1); psi(n) = 1;
     r = eye(NoS) - 2*psi*(psi');
