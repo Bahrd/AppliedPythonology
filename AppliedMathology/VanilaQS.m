c394907bbc66764029e8bf12d6bae068f835ff96
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-04-11 13:20:02 +0200

    Quantum Leap V

--- AppliedMathology/VanilaQS.m
+++ AppliedMathology/VanilaQS.m
@@ -8,7 +8,7 @@ Q = 10;
 NoS = 2^Q;                                               % A database size
 n = randi(NoS);                                          % A location of a marked state |n> 
                                                          % is randomly selected
-psi = @(n, NoS) [zeros(n - 1, 1); 1; zeros(NoS - n, 1)]; % The shortcut to a state constructor... 
+psi = @(n, NoS) [zeros(n - 1, 1); 1; zeros(NoS - n, 1)]; % The shortcut to a computational state constructor... 
 R =   @(n, NoS) eye(NoS) - 2*psi(n, NoS)*(psi(n, NoS)'); % ... and to a sign flipper R(n) = I - 2|n><n|
 
 H = hadamard(NoS)/sqrt(NoS);                             % A normalized Hadamard matrix
@@ -16,15 +16,15 @@ Rn = R(n, NoS); R1 = R(1, NoS);                          % Flippers
 G = -Rn * H * R1 * H;                                    % The Grover operator matrix
 
 %% Uno, due, tre... 
-q = floor(pi*sqrt(NoS)/4);                               % NUmber of Grover operator iterations
+q = floor(pi*sqrt(NoS)/4);                               % Number of Grover operator iterations
 
 % An iterative (more 'quantum implementable') version... 
-% (forgive the lack of the 1st/2n/third distinction please! ;)
+% [Ignore the lack of the 1st/2nd/3rd distinction please! ;]
 O = psi(1, NoS);
 phi = Rn * H * O;                                        % Initial step (inverting a phase of the marked state)
 for i = 1:q
     phi = G * phi;                                       % Step-by-step amplitude amplification 
-    sprintf('After %dth iteration P(No = %d) = %0.3g', i, n, phi(n)^2)
+    sprintf('P(No = %d) = %0.3g afer %dth iteration', n, phi(n)^2, i)
 end
 % or its 'en bloc genre'...
 phi = (G^q) * Rn * H * O;       
