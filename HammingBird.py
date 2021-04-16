8e6ba35d1c84b53ae78f5d03321f83e6de90a839
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-04-16 19:31:07 +0200

    Cuckoo's Nest O

--- HammingBird.py
+++ HammingBird.py
@@ -3,12 +3,12 @@
 # 'Hamming the humming data channel bits' [ We just couldn't help but play with these homophones!;]
 # Hummingbird moth is left...
 
-## A simple demonstration of the Hamming correction code
-# In the real world we would need to fit 2ⁿ - n bits of the message into 
-# 2ⁿ ones in locations between the dyadic ones: 2⁰,2¹,2²,…, 2ⁿ⁻¹).
-# Here we... don't! That's the trick (see the videos): 
-# 1. Create a random 2ⁿ-bit block and
-# 2. Make it a proper Hamming coded message (so that its Hamming syndrome is zero).
+## A simple demonstration of the Hamming(2ⁿ - 1, 2ⁿ - n - 1) correction code
+#  In the real world we would need to intertwin 2ⁿ - n bits of the message with 
+#  2ⁿ parity bits (located at dyadic locations: 2⁰, 2¹, 2², …, 2ⁿ⁻¹).
+#  Here we... don't! That's the trick (see the videos): 
+#  1. Create a random 2ⁿ-bit block and
+#  2. Make it a proper Hamming coded message (so that its Hamming syndrome is zero).
 from random import choice
 from functools import reduce
 
@@ -21,13 +21,13 @@ cuckoo = reduce(lambda x, y: x^y, idx)      # and XOR-ed to yield their Hamming 
                                             # aka the incorrect bit (cuckoo?) location
 nest[cuckoo] ^= 0b1                         # Kill [cuckoo] a mocking bird ... 
 # The 'nest' is now Hamming coded!          # Savvy? https://youtu.be/xG6RHY_WJpM?t=129
-print(f'{nest} - the correct block!')
+print(f'{nest} - correct {0b1 << n}-bit block!')
 
 
 
-# In order to emulate a humming-bird we flip a random bit
+# In order to emulate a humming-bit we flip a random bird
 brd = choice(range(0b1 << n)); nest[brd] ^= 0b1
-print(f'{nest} - the corrupted block!')
+print(f'{nest} - corrupted {0b1 << n}-bit block!')
 # To reveal the 'One [that] Flew Over the Cuckoo's Nest' straight away!
 idx = [i for i, b in enumerate(nest) if b]
 hmmng = reduce(lambda x, y: x^y, idx); print(f'Yippee-Ki-Yay {hmmng} == {brd}!')
