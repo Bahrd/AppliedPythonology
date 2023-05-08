9c9c9f869e5c8b78553462213d79c56541a61271
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2023-05-08 21:50:45 +0200

    De-sourcing

--- PolarAndPolygon.py
+++ PolarAndPolygon.py
@@ -1,9 +1,10 @@
 ﻿from numpy import cos, sqrt, linspace as lsp, reciprocal as rp
-from matplotlib.pyplot import plot, polar, title, show, gca, axis
+from matplotlib.pyplot import plot, polar, title, show, gca
 from math import pi as π
+from random import uniform as U
 
-##Polar square... and byenary 100 all!
-θ, ρ = lsp(0, π/0b100, 0b10), π/0o10 # ... or for octoplus!
+##Polar square ♪♫...and the byenary 100 all!♫♪
+θ, ρ = lsp(0, π/0b100, 0b10), U(π/0o10, π*0o1) # ... or for octoplus!
 r = rp(cos(θ))   
 for θ in (θ + n*π/0b10 for n in range(0b100)): 
     _ = polar(θ + ρ, r, θ + (π/0b100 + ρ), r[::-1]) 
@@ -17,4 +18,8 @@ for x, y in ((x, y), (x, -y), (-x, -y), (-x, y)):
     _ = plot(x, y, y, x)
 _ = title('Polygon circle'), show()
 
-### https://en.wikipedia.org/wiki/Parametric_equation#Some_sophisticated_functions
+''' 
+Source(s):
+1. https://en.wikipedia.org/wiki/Parametric_equation#Some_sophisticated_functions
+2. https://www.youtube.com/watch?v=AoOv6bWg9lk - "Polar Equations of Polygons"
+'''
