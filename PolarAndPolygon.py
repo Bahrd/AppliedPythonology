eb480ac3f43c308d516762607398d9c674fda718
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2023-05-16 09:34:53 +0200

    Make  π = π again!

--- PolarAndPolygon.py
+++ PolarAndPolygon.py
@@ -10,13 +10,17 @@ for θ in (θ + n*π/0b10 for n in range(0b100)):
     _ = polar(θ + ρ, r, θ + (π/0b100 + ρ), r[::-1]) 
 _ = title('Polar square'), show()
 
-#Polygon circle...
-x = lsp(0, sqrt(0b10)/0b10, 0x10) # Hexadecaphilia!
-y = sqrt(1 - x**0b10)
-gca().set_aspect('equal')
-for x, y in ((x, y), (x, -y), (-x, -y), (-x, y)):
-    _ = plot(x, y, y, x)
-_ = title('Polygon circle'), show()
+## Polygon Lp circles...
+#  p ~ 1.7915 (= 3583/2000) yields π ~ 3
+#  p = 2 makes π = π again!
+pp = (2, 1, 0.5, 0.25, 2048, 3583/2000)
+for p in pp:
+    x = lsp(0, pow(0o1/0b10, 1/p), 0x10) # Hexadecaphilia!
+    y = pow(1 - pow(x, p), 1/p)
+    gca().set_aspect('equal')
+    for x, y in ((x, y), (x, -y), (-x, -y), (-x, y)):
+        _ = plot(x, y, y, x)
+    _ = title(f'Polygon Lp, p = {p} circle'), show()
 
 ''' 
 Source(s):
