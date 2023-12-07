8cf626b6c59dc9ce3a2436b969f2daaa453f742c
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2023-12-07 23:54:54 +0100

    Kamil de Bézier... ;)

--- Bezier.py
+++ Bezier.py
@@ -2,28 +2,27 @@
 # https://www.pbr-book.org/4ed/Shapes/Curves
 
 import matplotlib.pyplot as plt
-from numpy import linspace as lp, matrix as mtx
+from numpy import linspace as lp
 from matplotlib.widgets import Button, Slider
-
 
 def bézier(p0, p1, p2, p3, n = 128):
     u = lp(0, 1, n)
     bx = p0[0]*(1 - u)**3 + 3*p1[0]*u * (1 - u)**2 + 3*p2[0]*(1 - u) * u**2 + p3[0]*u**3
     by = p0[1]*(1 - u)**3 + 3*p1[1]*u * (1 - u)**2 + 3*p2[1]*(1 - u) * u**2 + p3[1]*u**3
+    
     return bx, by
 
-P = [[0, 0], [.25, 0], [.75, .5], [1, 0]]
-pp = bézier(P[0], P[1], P[2], P[3])
+
+P = [[0, 0], [.25, -.5], [.75, .5], [1, 0]]
+pp, cp = bézier(*P), tuple(zip(*P))
 
 # Create the figure and the Bézier curve that we will manipulate
 fig, ax = plt.subplots()
-line, = ax.plot(pp[0], pp[1])
-points, = ax.plot([0, 0], 'k.')
+curve, points, = ax.plot(pp[0], pp[1], '-', cp[0], cp[1], 'k.')
 ax.set_xlabel('X'), ax.set_ylabel('Y'), 
 
 # adjust the main plot to make room for the sliders
 fig.subplots_adjust(left=0.25, bottom=0.25)
-
 # Make a horizontal slider to control the p1.X
 x_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03])
 x_slider = Slider(
@@ -34,7 +33,6 @@ x_slider = Slider(
     valinit=P[1][0],
     valstep=0.01
 )
-
 # Make a vertically oriented slider to control p1.Y
 y_ax = fig.add_axes([0.1, 0.25, 0.03, 0.65])
 y_slider = Slider(
@@ -48,15 +46,13 @@ y_slider = Slider(
 )
 
 # The function to be called anytime a slider's value changes
-def update(val):
-    P[1][0] = x_slider.val
-    P[1][1] = y_slider.val
+def update(_):
+    P[1][0], P[1][1] = x_slider.val, y_slider.val
+    pp, cp = bézier(*P), tuple(zip(*P))
     
-    pp = bézier(P[0], P[1], P[2], P[3])
-    line.set_xdata(pp[0])
-    line.set_ydata(pp[1])
+    curve.set_xdata(pp[0])
+    curve.set_ydata(pp[1])
 
-    cp = mtx([P[0], P[1], P[2], P[3]]).T
     points.set_xdata(cp[0])
     points.set_ydata(cp[1])
 
@@ -70,10 +66,9 @@ y_slider.on_changed(update)
 resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', hovercolor='0.975')
 
-def reset(event):
+def reset(_):
     x_slider.reset()
     y_slider.reset()
 button.on_clicked(reset)
 
-update(0.0)
-plt.show()
+plt.show() #... must go on!
