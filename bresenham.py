9260b8ed6b1ab83a21d5f5c182311a088ccbadfb
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2023-12-12 13:19:23 +0100

    The Bresenham's AA in ♪♫Black & White!♫♪

--- bresenham.py
+++ bresenham.py
@@ -3,9 +3,9 @@
 #  with our variations on an anti-aliasing theme...
 
 from numpy import clip, zeros, maximum, flipud
-from matplotlib.pyplot import imshow, show
+from matplotlib.pyplot import imshow, show, subplots
 
-def bresenham_line(x0: int, y0: int, x1: int, y1: int, antialiasing = True):
+def bresenham_line(x0: int, y0: int, x1: int, y1: int, AA:tuple or list = None):
     def clp(y: int):
         return clip(y, y0, y1)
 
@@ -13,27 +13,32 @@ def bresenham_line(x0: int, y0: int, x1: int, y1: int, antialiasing = True):
     D = 2*dy - dx
     
     line = zeros((dx, dy + 1))
-    y = y0
+    y, L = y0, len(AA) if AA != None else 0
+    c, HL = 1 - (L - 2*int(L/2)), int(L/2)
     for x in range(x0, x1):       
         if D > 0:
-            if(antialiasing):
-                for n, w in zip(range(y - 1, y + 3), (1/4, 1, 2/4, 1/4)):
+            if(AA != None):
+                for n, w in zip(range(y - HL + c, y - HL + c + L), AA):
                     line[x, clp(n)] = w
-            else:
-                line[x, y] = 1
+            else:   line[x, y] = 1
+        
             y = y + 1
             D = D - 2*dx
         else:
-            if(antialiasing):
-                for n, w in zip(range(y - 2, y + 2), (1/4, 2/4, 1, 1/4)):
+            if(AA != None):
+                for n, w in zip(range(y - HL, y - HL + L), AA[::-1]):
                     line[x, clp(n)] = w
-            else:
-                line[x, y] = 1
+            else:   line[x, y] = 1
+        
         D = D + 2*dy
     return line
 
-X, Y = 0x369, 0o137
-noa, aa = bresenham_line(0, 0, X, Y, False), bresenham_line(0, 0, X, Y)
-lines = maximum.reduce((flipud(noa), aa))
+#                       (1/3, 2/3, 1/1, 2/3, 1/3)   # Symmetric AA filters when L is odd...
+X, Y, AA = 0x163, 0o61, (1/2, 1/1, 2/3, 1/3)        # 'Odd' AAA (asymmetric AA) when L is not...
+noa, aa = bresenham_line(0, 0, X, Y, AA = AA), bresenham_line(0, 0, X, Y)
 
-_ = imshow(1 - lines.T, cmap='gray', interpolation = 'none'), show() # ... must gone!
+# A helper...
+duo_show = lambda data:tuple ((imshow(d, cmap='gray', interpolation = 'none'), 
+                               show()) for d in data)
+bresenhams = maximum.reduce((flipud(noa), aa))
+duo_show((1 - bresenhams.T, bresenhams.T))          # ... must go on and gone!
