7be2ccb3553bdbe6049f1e619f7506ba19df7b66
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2023-12-18 20:37:52 +0100

    A timestamp...

--- VisualPoissoning.py
+++ VisualPoissoning.py
@@ -10,15 +10,17 @@ from numpy import clip
 ## An animation part
 # https://stackoverflow.com/questions/33193696/matplotlib-animation-in-for-loop/41648966#41648966
 val = 8
+# A callback function
 def animate(_):
     global val, img
     
     λ = 2**(val - 8.0)        
     imp = clip(poisson(img * λ)/λ, 0x0, 0xff).astype(int)
+    # https://en.wikipedia.org/wiki/90482_Orcus#/media/File:Orcus-Vanth_orbit.gif
     im.set_array(imp)
     return im,
 
-# An infinite loop
+# An infinite loop provider
 def dummies():
     yield 1
 
@@ -29,11 +31,14 @@ def poissonimg(_val):
     val = _val
         
 def scotophotopic(label):
-    global img, jovi, slEV
+    global img, jovi
     img = cvtColor(jovi, RGB if label == 'RGB' else GRAY)
 
-# The main content
-fig, _ = plt.subplots(num = "Nihil novi sub Jovi..."); plt.subplots_adjust(bottom = .25)
+
+## The main content (Jupiter and Io, Europa, Ganymede & Callisto... 
+#                   (and the remaining (~0.003%) plankton))
+#  https://en.wikipedia.org/wiki/Orbital_resonance
+fig, _ = plt.subplots(num = "Nihil novi sub Jovi... [3.12.23 at 21:16:18]"); plt.subplots_adjust(bottom = .25)
 jovi = resize(imread('Jovi et consortes.JPG'), (0x140, 0o310)) # Good' ol' CGA...
 img = cvtColor(jovi, GRAY)
 im = plt.imshow(img, cmap = 'gray', interpolation = 'none')
@@ -43,5 +48,6 @@ axEV, axc = plt.axes([.25, .1, .65, .03]), plt.axes([.025, .01, .15, .15])
 slEV = Slider(axEV, 'EV', 0.0, 16.0, valinit = 8.0, valstep = 1.0); slEV.on_changed(poissonimg)
 radio = RadioButtons(axc, ('RGB', 'B&W'), active = 1); radio.on_clicked(scotophotopic)
 
+## Clap! https://en.wikipedia.org/wiki/Clapperboard#Construction
 anim = animation.FuncAnimation(fig, animate, frames = dummies, cache_frame_data = False, blit = True)
 plt.show(block = True)
