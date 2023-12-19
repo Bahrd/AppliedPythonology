db43ad17789e4e92cb29ce4d3e3109193ffb73f0
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2023-12-19 17:19:53 +0100

    ♪♫ With or without [them]♫♪

--- PoissonJupiter.py
+++ PoissonJupiter.py
@@ -12,15 +12,16 @@ A simple animation demonstrating Poisson distribution of faint images...
  https://en.wikipedia.org/wiki/Orbital_resonance
 '''
 
-## An animation part
+## A (vanila) animation
 intensity, brightness = 8, 0
-# A callback function
+# An alone callback function
 def animate(_):
     global intensity, im, img, brightness
     
     λ = 2**(intensity - 8.0)        
-    imp = clip(poisson(img * λ)/λ, 0x0, 0xff) * 2**brightness
+    imp = 2**brightness * clip(poisson(img * λ)/λ, 0x0, 0xff)
     im.set_array(imp.astype(int))
+
 
 ## A GUI part
 # A couple of event handlers
@@ -28,34 +29,41 @@ def poissonimg(_i):
     global intensity
     intensity = _i
    
-def brightning(_b):
+def brightnimg(_b):
     global brightness
     brightness = _b
     
-def scotophotopic(label):
+''' 
+People vs frogs (vs machines) - the world-famous single-photon detection contest
+1. https://www.sciencedaily.com/releases/2015/01/150120084545.htm
+2. https://www.scientificamerican.com/article/the-human-eye-could-help-test-quantum-mechanics/
+3. https://global.canon/en/technology/spad-sensor-2023.html
+4. https://www.youtube.com/watch?v=o5qXCzknxn8 [PG]
+'''
+def photoscotopic(label):
     global img, jovi
     img = cvtColor(jovi, RGB if label == 'RGB' else GRAY)
 
-
 ## The main content: Jupiter and Io, Europa, Ganymede & Callisto... 
-#                    [and the remaining (~0.003%) plankton]
-fig, _ = plt.subplots(num = "Nihil novi sub Jovi... [3.12.23 at 21:16:18]"); plt.subplots_adjust(bottom = .25)
+#                    [without the remaining (~0.003%) plankton]
+fig, _ = plt.subplots(num = "Nihil novi sub Jovi... [3.12.23 at 21:16:18]")
+plt.subplots_adjust(bottom = .25)
+
 jovi = resize(imread('Jovi et consortes.JPG'), (0x140, 0o310)) # Good' ol' CGA...
 img = cvtColor(jovi, GRAY)
 im = plt.imshow(img, cmap = 'gray', interpolation = 'none')
 
-# And an associated couple of elements
-axEV, axB, axc = plt.axes([.25, .1, .65, .03]), plt.axes([.92, .25, .03, .65]), plt.axes([.025, .01, .15, .15])
+## And a handful of elements
+#  A pair of sliders...
+axEV, axB, axc = (plt.axes(dims) for dims in ([.25, .1, .65, .03], [.92, .25, .03, .65], [.025, .01, .15, .15]))
+slEV, slB = (Slider(axEV, 'EV', 0, 16, valinit = intensity, valstep = 1), 
+             Slider(axB, 'Brightness', -4, 1, orientation = 'vertical', valinit = brightness, valstep = 0.25))
+slEV.on_changed(poissonimg), slB.on_changed(brightnimg)
 
-slEV = Slider(axEV, 'EV', 0, 16, valinit = 8, valstep = 1)
-slEV.on_changed(poissonimg)
-
-slB = Slider(axB, 'Brightness', -4, 0, orientation = 'vertical', valinit = 0, valstep = 0.25)
-slB.on_changed(brightning)
-
+# ... and a switch
 radio = RadioButtons(axc, ('RGB', 'B&W'), active = 1)
-radio.on_clicked(scotophotopic)
+radio.on_clicked(photoscotopic)
 
 ## "Lights, Camera, Action" https://en.wikipedia.org/wiki/Clapperboard#Construction
 anim = animation.FuncAnimation(fig, animate, cache_frame_data = False)
-plt.show(block = True)
+plt.show()
