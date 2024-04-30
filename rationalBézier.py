620959595b58d91abdcf4cef16c725faf1f6016c
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-04-30 12:30:47 +0200

    Rationale behind π & φ... with deBézier

--- rationalBézier.py
+++ rationalBézier.py
@@ -2,28 +2,36 @@
 # TBI (to be implemented)...
 
 import matplotlib.pyplot as plt
-from numpy import linspace as lp, outer as op, tensordot as td
+from numpy import linspace as lp, outer as op, tensordot as tp, multiply as hp, reciprocal as rp
 from matplotlib.widgets import Button, Slider
 
 
-def rationalBézier(P, p = 128):
+def rationalBézier(P, w, p = 128):
     def binomial(n, k):  
        return 1 if k == 0 or n == k else binomial(n - 1, k - 1) + binomial(n - 1, k)
     
     n, u = len(P), lp(0, 1, p)
-    c, b = zip(*((binomial(n - 1, i), op(P[i], (1 - u)**(n - i - 1) * u**i)) for i in range(n)))
+    
+    Pw = hp(P, w)
+    c, b = zip(*((binomial(n - 1, i), op(Pw[i], (1 - u)**(n - i - 1) * u**i)) for i in range(n)))
+    pp = tp(c, b, 1)
+    
     '''
-    Rational part will appear right here (but not right now!)
+    Rationale = Bézier/deBézier
     '''
-    return td(c, b, 1)
+    e, d = zip(*((binomial(n - 1, i), op(w[i], (1 - u)**(n - i - 1) * u**i)) for i in range(n)))
+    dp = tp(e, d, 1)
+    pp[0], pp[1] = hp(pp[0], rp(dp[0])), hp(pp[1], rp(dp[0]))
+    return pp
 
-# A pair of the endpoints and the control ones ((usually) between them)
-P = [[0, 0], [1/8, -1], [1/4, 1], [1/2, -1], [3/4, 1], [1, 0]]
-pp, cp = rationalBézier(P), tuple(zip(*P))
+# A pair of the endpoints and the control one between them)
+P = [[0.0, 0], [1/2, 1], [1, 0]]
+w = [[1, 1], [1, 1], [1, 1]]
+pp, cp = rationalBézier(P, w), tuple(zip(*P))
 
 # Create the figure and a Bézier curve that we will manipulate
 fig, ax = plt.subplots(num = "Rational Bézier curve demo"); plt.tight_layout()
-curve, points, = ax.plot(pp[0], pp[1], '-', cp[0], cp[1], 'k.')
+curve, points = ax.plot(pp[0], pp[1], '-', cp[0], cp[1], 'k.')
 ax.set_xlabel('X'), ax.set_ylabel('Y')
 
 # Adjust the main plot to make room for the sliders
@@ -39,18 +47,26 @@ y_slider = Slider(label = 'p₁.Y',
     ax = y_ax, orientation = "vertical",
     valmin = -1, valmax = 2, valinit = P[1][1], valstep = 0.01)
 
+w_ax = fig.add_axes([0.96, 0.25, 0.0125, 0.6125])
+w_slider = Slider(label = 'p₁.w',
+    ax = w_ax, orientation = "vertical",
+    valmin = -2, valmax = 2, valinit = w[1][1], valstep = 0.01)
+
 # The function to be called anytime a slider's value changes
 def update(_):
     global fig, x_slider, y_slider, P, curve, points
-    P[1][0], P[1][1] = x_slider.val, y_slider.val
-    pp, cp = rationalBézier(P), tuple(zip(*P))
-
+    P[1][0], P[1][1], w[1][1] = x_slider.val, y_slider.val, w_slider.val
+    w[0][1] = w[1][1]
+   
+    pp, cp = rationalBézier(P, w), tuple(zip(*P))
+        
     curve.set_xdata(pp[0]),  curve.set_ydata(pp[1])
     points.set_xdata(cp[0]), points.set_ydata(cp[1])
     fig.canvas.draw_idle()
 
 # Register the update function with each slider
-x_slider.on_changed(update), y_slider.on_changed(update)
+x_slider.on_changed(update), y_slider.on_changed(update), w_slider.on_changed(update)
+
 
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
 resetax = fig.add_axes([0.03125, 0.05, 0.1, 0.04])
