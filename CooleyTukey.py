3fcda5182c2ad3b55639bad7b69a47728307fe23
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-05-13 21:26:08 +0200

    Cooley and Tukey - how cool is that?

--- CooleyTukey.py
+++ CooleyTukey.py
@@ -1,5 +1,5 @@
 ﻿from matplotlib.pyplot import bar, plot, subplot, figure, tight_layout, show, xlabel, ylabel, title
-from numpy import exp, sin, cos, concatenate, arange, abs, pi as π, linspace as lp
+from numpy import exp, sin, concatenate, arange, abs, pi as π, linspace as lp
 def CooleyTukey(x):
     """
     A recursive implementation of the 1D Cooley-Tukey FFT
@@ -17,17 +17,17 @@ def CooleyTukey(x):
     
 N = 0x100
 # time, frequencies and phases
-t, (f1, f2, f3), (φ1, φ2, φ3) = lp(0, 1, N), (1<<3, 1<<4, 1<<5), (π/2, π/3, π/4)
-#from random import random as rr, randrange as ri
-#t, (f1, f2, f3), (φ1, φ2, φ3) = lp(0, 1, N), (1<<ri(3), 1<<ri(4), 1<<ri(5)), (2*rr()*π, 2*rr()*π, 2*rr()*π)
+# t, (f1, f2, f3), (φ1, φ2, φ3) = lp(0, 1, N), (1<<3, 1<<4, 1<<5), (π/2, π/3, π/4)
+from random import random as rr, randrange as ri
+t, (f1, f2, f3), (φ1, φ2, φ3) = lp(0, 1, N), (1<<ri(3), 1<<ri(4), 1<<ri(5)), (2*rr()*π, 2*rr()*π, 2*rr()*π)
 
-# A signal... 
-x = sin(2*f1*π*t + φ1) - cos(2*f2*π*t + φ2) + sin(2*f3*π*t+ φ3)
-# ... and its FFT
+# A slow signal... 
+x = sin(2*f1*π*t + φ1) + sin(2*f2*π*t + φ2) + sin(2*f3*π*t + φ3)
+# ... and its fast transform
 X = CooleyTukey(x)
 
 # Plotting
-figure(figsize=(10, 6))
+figure(figsize = (10, 6))
 subplot(2, 1, 1)
 plot(t, x)
 title("Input Signal"); xlabel("Time"); ylabel("Amplitude")
