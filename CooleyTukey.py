3c2464b022c12545ffff2f81ef9300dff008bbc8
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-05-14 09:05:05 +0200

    DFT or There and Back Again

--- CooleyTukey.py
+++ CooleyTukey.py
@@ -4,31 +4,43 @@ from random import random as rr, randrange as ri
 from auxiliary import ITT
 
 
-## DFT (Discrete Fourier Transform) without FFT
-# https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter24.02-Discrete-Fourier-Transform.html
 @ITT
 def DFT(x):
+    '''
+    A neat matrix multiplication-based implementation of the 1D DFT [via Copilot I]
+    # https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter24.02-Discrete-Fourier-Transform.html
+    '''
     N = len(x)
     n = arange(N); ω = n.reshape((N, 1))
-    W = exp(-2j * π * ω * n / N)
+    W = exp(-2j * π * ω * n/N)
+    
+    return W @ x
+
+@ITT
+def IDFT(x):
+    '''
+    A neat matrix multiplication-based implementation of the 1D DFT [via Copilot I]
+    # https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter24.02-Discrete-Fourier-Transform.html
+    '''
+    N = len(x)
+    n = arange(N); ω = n.reshape((N, 1))
+    W = exp(2j * π * ω * n/N)
     
     return W @ x
 
 @ITT
 def CooleyTukey(x):
-    """
-    A neat recursive implementation of Cooley'n'Tukey's 1D FFT
+    '''
+    A neat recursive implementation of Cooley'n'Tukey's 1D FFT [via Copilot II]
     https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter24.03-Fast-Fourier-Transform.html 
-    [via Copilot]
-    """
+    '''
     def _CooleyTukey(x):
         N = len(x)
         if N > 1:
             E, O = _CooleyTukey(x[::2]), _CooleyTukey(x[1::2])
             n = arange(N)
-            ω = exp(-2j * π * n/N)
+            ω = exp(-2j * π * n/N); N >>= 1
             
-            N = N >> 1
             return ConcaTenate([E + ω[:N] * O, E + ω[N:] * O])
         else:
             return x
@@ -46,16 +58,20 @@ _X, X = DFT(x), CooleyTukey(x)
 
 # Plotting
 figure(figsize = (10, 0b110))
-subplot(3, 1, 1)
+subplot(4, 1, 1)
 plot(t, x, color = 'red')
-title("Input Signal"); xlabel("Time"); ylabel("Amplitude")
+title("Slow signal"); xlabel("Time"); ylabel("Amplitude")
 
-subplot(3, 1, 2)
+subplot(4, 1, 2)
 bar(arange(N), abs(X), color = 'brown', width = 1.0)
-title("FFT Spectrum"); xlabel("Frequency Bin"); ylabel("Magnitude")
+title("Fast transform"); ylabel("Magnitude")
 
-subplot(3, 1, 3)
+subplot(4, 1, 3)
 bar(arange(N), abs(_X), color = 'black', width = 1.0)
-title("DFT Spectrum"); xlabel("Frequency Bin"); ylabel("Magnitude")
+title("Slow transform"); xlabel("Frequencies [Hz]"); ylabel("Magnitude")
+
+subplot(4, 1, 4)
+plot(arange(N), IDFT(_X), color = 'red')
+title("Slow signal"); xlabel("Time"); ylabel("Amplitude")
 
 tight_layout(); show()
