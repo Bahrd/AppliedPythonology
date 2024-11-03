8d7ecbf736155b878285e3181f44fde04b5992ba
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-11-03 12:42:13 +0100

    Premature octomization is the root of all... fun!

--- bresenhamCircle.py
+++ bresenhamCircle.py
@@ -4,14 +4,16 @@ from numpy import ones, fliplr, flipud, arange
 from matplotlib.pyplot import imshow, show, pause, subplot, figure
 from numpy.random import choice, permutation as rpr
 
+# A shortcut for a square matrix of ones
+sqm = lambda _r: ones((_r, _r))
 ## Shades of gray... (or pale, if you like Procol Harum): https://www.youtube.com/watch?v=xM_N2O-gzP4
-shades = (0/0x100, 0x69/0x100, 0x80/0x100, 0xd3/0x100, # black, dimgray, gray, lightgray, etc: https://en.wikipedia.org/wiki/Grey#Optics
+shades = (0/0x100, 0x69/0x100, 0x80/0x100, 0xd3/0x100,    # black, dimgray, gray, lightgray, etc: https://en.wikipedia.org/wiki/Grey#Optics
           0xc0/0x100, 0xdc/0x100, 0xa9/0x100, 0xbb/0x100) # silver, gainsboro, darkgray, and "pale"...
 
 ## A plain (but not quite pale!) quadrant version
 #  http://members.chello.at/~easyfilter/bresenham.html
 def bresenham_quadrant_circle(_r: int):
-    Φ = ones((2*_r + 1, 2*_r + 1))
+    Φ = sqm(2*_r + 1)
     ζ, ξ, x, y, ε = _r, _r , -_r, 0, 2 - 2*_r
     
     while x <= 0:                   
@@ -24,30 +26,6 @@ def bresenham_quadrant_circle(_r: int):
         if r > x or ε > y:                  
             x += 1; ε += 2*x + 1            
     return Φ                                
-
-# And an even smarter (octant) one: 
-# https://www.youtube.com/watch?v=hpiILbMkF9w
-def bresenham_octant_circle(_r: int):
-    Φ = ones((2*_r + 1, 2*_r + 1))
-    ζ, ξ = _r, _r
-    x, y, ε = 0, -_r, -_r
-    
-    while x < -y:
-        if ε > 0:
-            y += 1
-            ε += 1 + 2*(x + y)
-        else:
-            ε += 1 + 2*x
-
-        for (n, m), o in zip(((ζ + x, ξ + y), (ζ - x, ξ - y), 
-                              (ζ + x, ξ - y), (ζ - x, ξ + y), 
-                              (ζ + y, ξ + x), (ζ - y, ξ - x), 
-                              (ζ + y, ξ - x), (ζ - y, ξ + x)), 
-                             shades):   
-            Φ[n, m] = o 
-        x += 1
-    return Φ
-    
 ## A version obscured by debuggin' code (inactive if '-O' option is on)
 def dbresenham_quadrant_circle(_r: int):
     Φ = ones((2*_r + 1, 2*_r + 1))
@@ -55,8 +33,8 @@ def dbresenham_quadrant_circle(_r: int):
     
     c, _c = 0, False                #  Bookkeeping Q1 [counting points of a circumference]
     while x <= 0:                                       
-        for (n, m), o in zip(((ζ - x, ξ + y),   (ζ - y, ξ - x),         # ♪♫ One for the money, two for the show;  
-                            (ζ + x, ξ - y), (ζ + y, ξ + x)), shades):   #    Three to make ready, and four to go! ♫♪ https://genius.com/Carl-perkins-blue-suede-shoes-lyrics
+        for (n, m), o in zip(((ζ - x, ξ + y), (ζ - y, ξ - x),           # ♪♫ One for the money, two for the show;  
+                              (ζ + x, ξ - y), (ζ + y, ξ + x)), shades): #    Three to make ready, and four to go! ♫♪ https://genius.com/Carl-perkins-blue-suede-shoes-lyrics
             Φ[n, m] = o                                                 # https://en.wikipedia.org/wiki/One_for_the_Money
                                                                         # https://youtu.be/Bm5HKlQ6nGM?t=8 - by E. Presley '65
         r = ε                                                           # https://www.youtube.com/watch?v=O6BbL4DrrBo      '68
@@ -73,13 +51,40 @@ def dbresenham_quadrant_circle(_r: int):
     print(f'For r = {_r}, we have 2πr = {4*c} and π = {2*c/_r:.2f}...',
             f'(or d = {2*_r + 1}, πd = {4*c} and thus π = {4*c/(2*_r + 1):.2f} ;)')  
     return Φ
-
-r = choice(range(0o1, 0o17))
-bresenham_circle = dbresenham_quadrant_circle if __debug__ else choice((bresenham_quadrant_circle, bresenham_octant_circle))
+# And an even smarter (octant) one (written in a "binary" style! ;): 
+# https://www.youtube.com/watch?v=hpiILbMkF9w
+def bresenham_octant_circle(_r: int):
+    # A canvas 0b100 the circle...
+    Φ = sqm((_r << 1) + 1)
+    ζ, ξ = _r, _r
+    
+    #... and the actual algorithm
+    x, y, ε = 0, _r, _r
+    while x < y:
+        if ε < 0:
+            y -= 1
+            ε -= x - y << 1
+        else:
+            ε -= x << 1
+        # ... Octavian, octant, octopus, octet (a.k.a. byte), October(!) for octo(genarians?!)... 
+        for (n, m), o in zip(((ζ + x, ξ + y), (ζ - x, ξ - y), (ζ + x, ξ - y), (ζ - x, ξ + y), 
+                              (ζ + y, ξ + x), (ζ - y, ξ - x), (ζ + y, ξ - x), (ζ - y, ξ + x)), 
+                             shades):   
+            Φ[n, m] = o 
+        x += 1; ε -= 1
+    return Φ
+    
+## ♪♫ Che sarà sarà... ♫♪ https://youtu.be/i9nWB5XifBI (a terrific induction/recursion...)
+# Primus aleus..
+r = choice(range(0o1, 0o17)) 
+# ... et aleus secundus!
+bresenham_circle = dbresenham_quadrant_circle if __debug__ else choice((bresenham_quadrant_circle, 
+                                                                        bresenham_octant_circle))
+# Alea iacta est! 
 bc = bresenham_circle(r)
 
-fig = figure(figsize = (10, 3));subplot(1, 3, 1); 
 ## Drawing on a grid...
+fig = figure(figsize = (10, 3));subplot(1, 3, 1); 
 #  https://www.tutorialspoint.com/remove-the-x-axis-ticks-while-keeping-the-grids-matplotlib    
 ax = fig.gca()
 ax.set_xticks(arange(.5, 2*r + 1, 1)), ax.set_yticks(arange(.5, 2*r + 1, 1))
@@ -88,7 +93,7 @@ ax.grid(True)
 
 imshow(bc, cmap = 'gray', interpolation = 'none')
 
-## A bit of low-level bit-blitting...
+## A bit of 3L2B (low-level-like bit-blitting)...
 #  https://en.wikipedia.org/wiki/Blitter
 v, w = 2*r + 4, 2*r + 1
 
@@ -102,21 +107,21 @@ canvas[1:1 + w, v:v + w] = flipud(fliplr(bc))
 subplot(1, 3, (2, 3)); im = imshow(canvas, cmap = 'gray', interpolation = 'none')
 u = 0, .33
 
-## Fill the void(s)... (kinda exhaustive search?)
-#  https://en.wikipedia.org/wiki/Flood_fill #Stack-based_recursive_implementation_(four-way)
-#  Had I been you, I would've used a quad-tree... https://en.wikipedia.org/wiki/Quadtree
-#  A kindergarten random walk version: ♪♫Viva, Las Vegas!♫♪ https://youtu.be/uxmx9AV1GKU
-#  https://www.pbr-book.org/4ed/Monte_Carlo_Integration
+r''' Fill the void(s)... (kinda exhaustive search?)
+  https://en.wikipedia.org/wiki/Flood_fill #Stack-based_recursive_implementation_(four-way)
+  Had I been you, I would've used a quad-tree... https://en.wikipedia.org/wiki/Quadtree
+  A kindergarten random walk version: ♪♫Viva, Las Vegas!♫♪ https://youtu.be/uxmx9AV1GKU
+  https://www.pbr-book.org/4ed/Monte_Carlo_Integration '''
 def flood_fill(x: int, y: int):
     global im, canvas, u
     
-    canvas[x, y] = choice(shades) #uniform(*u)  # For each pixel is "drowned" once    
+    canvas[x, y] = choice(shades)   # For each pixel is "drowned" once    
     im.set_array(canvas), fig.canvas.draw_idle(), pause(0.001)
         
     # A fourfold (↑, ←, ↓, →) randomly ordered recurrence
     for (x, y) in rpr(((x - 1, y), (x, y - 1),   # ♪♫ [Never] the same, playin' your game.
                        (x + 1, y), (x, y + 1))): #    Drive me insane, trouble is gonna come to you... ♫♪
-        if canvas[x, y] == 1: flood_fill(x , y)            
+        if canvas[x, y] == 1: flood_fill(x , y)  # https://youtu.be/PNk8NBqCEN8          
     
 ## "Sī fuerīs Rōmae, Rōmānō vīvitō mōre; sī fuerīs alibī, vīvitō sīcut ibī..."
 #  Or... just make sure the starting point (x, y) is inside the area
@@ -127,7 +132,7 @@ flood_fill(x = 0b10, y = 0o10)
 #flood_fill(25, 25), flood_fill(25, 50)
 show()
 
-''' #'Run with the hare and hunt with the hounds'
+r''' #'Run with the hare and hunt with the hounds'
     # https://sl.bing.net/hmIR3ZJE4a
     
 def flood_fill(x: int, y: int):
--- bresenhamLine.py
+++ bresenhamLine.py
@@ -9,45 +9,49 @@ from matplotlib.pyplot import imshow, show, subplot, tight_layout
 
 ## See this "esplanation": https://youtu.be/CceepU1vIKo?t=20 first!
 def bresenham_line(x0: int, y0: int, x1: int, y1: int):   
-    dx, dy = x1 - x0, y1 - y0
-    D = (dy << 1) - dx
-    
-    Λ = zeros((dx, dy + 1))
     y = y0
-    dx <<= 1; dy <<= 1
+    Δx, Δy = x1 - x0, y1 - y0   # d([xy]) → Δ$1
+    D = (Δy << 1) - Δx
+    
+    Λ = zeros((Δx, Δy + 1))
+    Δx <<= 1; Δy <<= 1
     for x in range(x0, x1):       
         Λ[x, y] = 1
         if D > 0:
-            y += 1
-            D -= dx
-        D += dy
+            y += 1; D -= Δx
+        D += Δy
     return Λ
 
+## You should rather prefer a more mature approach to anti-aliasing...
+#  https://www.youtube.com/watch?v=f3Rs20k-hcI
+#  which is, however, not that downsampling-proof
 def bresenhamAAline(x0: int, y0: int, x1: int, y1: int, AA:Iterable):
     def Γ(y: int):
         return clip(y, y0, y1)
     
-    dx, dy = x1 - x0, y1 - y0
-    D = 2*dy - dx
+    Δx, Δy = x1 - x0, y1 - y0
+    D = (Δy << 1) - Δx
     
-    Λ = zeros((dx, dy + 1))
+    Λ = zeros((Δx, Δy + 1))
     y, L = y0, len(AA)
-    c, HL = 1 - (L - 2*(L//2)), (L//2)
+    c, HL = 1 - (L % 0b10), L >> 1
     for x in range(x0, x1):       
         if D > 0:
-            for n, w in zip(range(y - HL + c, y - HL + c + L), AA): Λ[x, Γ(n)] = w        
-            y = y + 1
-            D = D - (dx << 1)
+            for n, w in zip(range(y - HL + c, y - HL + c + L), AA): 
+                Λ[x, Γ(n)] = w        
+            y += 1
+            D -= Δx << 1
         else:
-            for n, w in zip(range(y - HL, y - HL + L), AA[::-1]): Λ[x, Γ(n)] = w
+            for n, w in zip(range(y - HL, y - HL + L), AA[::-1]): 
+                Λ[x, Γ(n)] = w
       
-        D = D + (dy << 1)
+        D += Δy << 1
     return Λ
 
 #                       (1/3, 2/3, 1/1, 2/3, 1/3)   # Symmetric AA filters when L is odd...
 #                       (1/2, 1/1, 2/3, 1/3)        # 'Odd' AAA (asymmetric AA) when L is not...
 X, Y, AA = 0x161, randint(0o161), (1/2, 1/1, 1/2)   # A minimal AA example...
-noa, aa = bresenhamAAline(0, 0, X, Y, AA = AA), bresenham_line(0, 0, X, Y)
+noa, aa = bresenham_line(0, 0, X, Y), bresenhamAAline(0, 0, X, Y, AA)
 
 # A helper...
 def subshow (data:tuple):
@@ -57,5 +61,5 @@ def subshow (data:tuple):
         imshow(d, cmap = 'gray', interpolation = 'none')
     show()
     
-bresenhams = maximum.reduce((flipud(noa), aa))
+bresenhams = maximum.reduce((flipud(aa), noa))
 subshow((1 - bresenhams.T, fliplr(bresenhams.T)))          # ... must go on and gone!
