686598b0a487d75b0d896ee290cc189df8857322
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-11-12 08:27:35 +0100

    Bye, bye, binary?

--- deBézier2D.py
+++ deBézier2D.py
@@ -4,8 +4,8 @@ import numpy as np
 from random import random
 from matplotlib import pyplot as plt
 
-## Pretentious homonyms mode ON...
-def deBézierFace(samples, steps = 128):
+## Pretentious homonyms/typos mode 0N(E)...
+def deBézierFace(samples, steps = 0x100):
    binomial = lambda n, k: 1 if k == 0 or n == k else binomial(n - 1, k - 1) + binomial(n - 1, k)
    BernsteinPloy = lambda i, n, t: binomial(n, i) * (t ** i) * ((1 - t) ** (n - i))
 
@@ -13,24 +13,25 @@ def deBézierFace(samples, steps = 128):
    U, V = np.meshgrid(*uv)
    n, m, _ = samples.shape
    
-   sureface = np.zeros((steps, steps, 3))
+   sureface = np.zeros((steps, steps, 0b11))
    for i in range(n):
       for j in range(m):
          sureface += np.outer(BernsteinPloy(i, n - 1, U) * BernsteinPloy(j, m - 1, V),
-                              samples[i, j]).reshape(steps, steps, 3)
+                              samples[i, j]).reshape(steps, steps, 0b11)
    return sureface
 
-# An NxM grid of samples of a surface
-N, M = 8, 16
-samples = np.array([[[i, j, 2*random() - 1] for i in range(N)] for j in range(M)])
-sure_face = deBézierFace(samples)
+# An NxM grid of samples of a surely random surface
+N, M = 0o10, 0x10
+samples = np.array([[[i, j, random()] for i in range(N)] for j in range(M)])
+sureface = deBézierFace(samples)
 
 # Go, figure!
 ax = plt.figure().add_subplot(111, projection = '3d')
 plt.tight_layout()
-ax.plot_surface(*(sure_face[:, :, d] for d in range(3)), cmap = 'terrain') # À la da Vinci: https://en.wikipedia.org/wiki/Hypsometric_tints#History
-ax.set_title("Bizarre de Bézier's decor")
-# Reflections anyone?
+ax.plot_surface(*(sureface[:, :, d] for d in range(0b11)), cmap = 'terrain') # À la da Vinci: https://en.wikipedia.org/wiki/Hypsometric_tints#History
+ax.set_title("Bézier's Bizarre Background")
+# "Reflections?" Anyone?
 for c, l in zip(('x', 'y', 'z'), ('Longitude', 'Latitude', 'Altitude')):
      eval(f'ax.set_{c}label')(l)
 plt.show()
+#  ... and ON, and 0N, and ΘN!
