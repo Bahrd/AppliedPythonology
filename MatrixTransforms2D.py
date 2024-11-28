140e422e34667f114c23b3551a0615fe0994a402
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-11-28 18:43:03 +0100

    Mike Oldfield's tribute to Évariste Galois...

--- MatrixTransforms2D.py
+++ MatrixTransforms2D.py
@@ -11,6 +11,7 @@ h2e = lambda X: array([(x/x[-1])[:-1] for x in X])
 # Set the stage!
 # https://www.geeksforgeeks.org/how-to-draw-shapes-in-matplotlib-with-python/
 fig, ax = plt.subplots()
+ax.set_xlim(-1, 4); ax.set_ylim(-1, 4); ax.set_aspect('equal'); plt.grid(True)
 
 # The original square (with a kink) at the origin
 sqwk = array([[0, 0, 1], [1, 0, 1], [1, 1, 1], [.25, .25, 1], [0, 1, 1]])
@@ -23,28 +24,28 @@ polygon = Polygon(vertices, ec = 'green', fc = 'lightgreen', alpha = 0.5)
 ax.add_patch(polygon); ax.scatter(vertices[:, 0], vertices[:, 1], color = 'green', s = 25)
 
 # Our square after translation by K and L...
-K, L = 2, 1
+K, L = 1.5, 1
 T = array([[1, 0, K],
            [0, 1, L],
            [0, 0, 1]])
 # ... and after rotation by α degrees...
-α = 45*π/180
+α = -60*π/180
 R = I(3); _R = array([[cos(α), -sin(α)],
                       [sin(α),  cos(α)]])
 R[:2, :2] = _R
 
 # ... and scaling...
-s = 2
+s = .75
 S = I(3); _S = array([[s, 0],
                       [0, s]])
 S[:2, :2] = _S
 
-## And so on...
+## And so on... Procrustes would've been proud of us!
 #  See https://en.wikipedia.org/wiki/Transformation_matrix#/media/File:2D_affine_transformation_matrix.svg
 #  and https://en.wikipedia.org/wiki/Transformation_matrix#/media/File:Perspective_transformation_matrix_2D.svg
 
 # Stretching:
-k, κ = 2, 1
+k, κ = 2, 2
 S1 = array([[k, 0, 0],
             [0, κ, 0],
             [0, 0, 1]])
@@ -58,7 +59,7 @@ S3 = array([[1, k, 0],
             [0, 0, 1]])
 
 # Reflecting:
-lx, ly = 1, 1   # ... about the line from origin through (lx, ly)
+lx, ly = 2, 1   # ... about the line from origin through (lx, ly)
 M = I(3)
 _M = array([[lx**2 - ly**2,     2*lx*ly  ],
             [    2*lx*ly  , ly**2 - lx**2]])
@@ -66,24 +67,29 @@ _M = _M/(lx**2 + ly**2)
 M[:2, :2] = _M
 
 # Feel free to further squeeze, stretch, shear the shape (and mix the order too)...
-sqwk = transform(M@T@S@R, sqwk); vertices = h2e(sqwk)
+sqwk = transform(M@T@S@R@S3, sqwk); vertices = h2e(sqwk)
 polygon = Polygon(h2e(sqwk), ec = 'blue', fc = 'lightblue', alpha = 0.5)
 
 ax.add_patch(polygon); ax.scatter(vertices[:, 0], vertices[:, 1], color = 'blue', s = 25)
 
-# ... and, eventually, after the final projection (a.k.a. casting a shadow)...
+# ... and, eventually, the final projection (a.k.a. casting a shadow)...
 # https://youtu.be/27vT-NWuw0M, https://youtu.be/JK-8XNIoAkI and https://youtu.be/cTyNpXB92bQ
-# which is a linear operation and hence can be represented by a matrix:
+# which is a linear operation too and can be represented by a matrix as well:
 # https://wrfranklin.org/pmwiki/Main/HomogeneousCoords
-
 # The shadow of the final shape on the line Ax + By = 1
 A, B = 1, 1
 P = array([[1, 0, 0],
            [0, 1, 0],
            [A, B, 0]])
-sqwk = transform(P, sqwk); vertices = h2e(sqwk)
+shdw = transform(P, sqwk); vertices = h2e(shdw)
 polygon = Polygon(vertices, ec = 'red', fc = 'tomato', alpha = 0.5)
-
 ax.add_patch(polygon); ax.scatter(vertices[:, 0], vertices[:, 1], color = 'red', s = 25)
-ax.set_xlim(-1, 4); ax.set_ylim(-1, 4); ax.set_aspect('equal'); plt.grid(True)
-plt.title('Matrix Transforms 2D'); plt.show()
+
+# And the ♪♫Moonlight shadows♫♪... https://youtu.be/ixExC-Zgyzc and https://youtu.be/e80qhyovOnA of the vertices. 
+# Is that song about Évariste Galois https://en.wikipedia.org/wiki/%C3%89variste_Galois#Final_days?
+for v in sqwk:
+    # Homogeneous to Euclidean coordinates
+    x, y = v[:-1]/v[-1]
+    ax.plot((0, x), (0, y), c = 'lightgray', lw = .5, ls = 'dashed')
+
+plt.title('Matrix transforms in 2D: rigid, linear, affine & projective'); plt.show()
