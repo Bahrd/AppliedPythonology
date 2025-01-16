ac094b384bc6d4232092c0b7d68cc3cb22e6032e
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2025-01-16 09:00:30 +0100

    Interactive awareness

--- J2K.py
+++ J2K.py
@@ -1,8 +1,7 @@
 #%% Vanilla JPEG 2000 algorithm
-# (the vanillin one, in fact: neither EBCOT nor BAC whatsoever)
+# (the vanillin one, in fact: neither EBCOT nor BAC is implemented whatsoever)
 import cv2 as openCV; import numpy as np
 from itertools import repeat
-from sys import argv as family
 #%% https://pywavelets.readthedocs.io/en/latest/ref/dwt-coefficient-handling.html
 from pywt import dwt2, idwt2
 from pywt import (wavedec2 as fwt2, waverec2 as ifwt2, threshold as thrsd,
@@ -36,10 +35,14 @@ art = 'GrassHopper'    # pseud. Philip [gr. "friend of horses"]!... ;)
 
 #%% JPEG 2000 main 'codec'
 # Wavelet transforms' (hiper-)parameters
+#  Hard thresholding:             'lambda x, T: thrsd(x, T, mode = 'hard')'
+L, Q, qntz = 4, -6, lambda x, Q: np.floor(x*2**Q + .5)/2**Q
+# Interactive-aware wavelet family name selection
 # https://en.wikipedia.org/wiki/Cohen-Daubechies-Feauveau_wavelet#Numbering
 # 'bior1.1', 'bior2.2', 'bior4.4' = 'Haar', 'LGT 5/3', 'CDF 9/7'
-#  Hard thresholding:             'lambda x, T: thrsd(x, T, mode = 'hard')'
-L, wn, Q, qntz = 4, 'bior2.2', -6, lambda x, Q: np.floor(x*2**Q + .5)/2**Q
+import sys;
+if hasattr(sys, 'ps1'): wn = 'bior4.4'
+else:                   wn = sys.argv[1] if len(sys.argv) > 1 else 'bior2.2'
 
 img = openCV.imread(f'./images/{art}.png')
 DI([img[..., n] for n in (2, 1, 0)], ['R', 'G', 'B'], grid = False)
