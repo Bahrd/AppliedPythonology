0178620004ff4dbdad04c8ccaeae207a9c7f9a5a
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2025-01-22 22:02:18 +0100

    That's all, folks!

--- WaveletMRA.py
+++ WaveletMRA.py
@@ -6,7 +6,8 @@
 #   Some say: "You can choose your friends, but you can't choose your family!"
 #   Oh, really?! :D
 
-import matplotlib.pyplot as plt; import sys; import numpy as np
+import matplotlib.pyplot as plt; import sys
+from numpy import array, concatenate as cnc, clip
 
 from auxiliary import displayImages as di, RGB2YCoCg as RGB2YCxCy # RGB2YCbCr
 from imports.plotWindow import plotWindow as mtw
@@ -15,42 +16,40 @@ from pywt import dwt2, idwt2
 
 ##  The "GrassHopper" is a "friend of horses" (Philip) and a "spirit" (Mustang)...
 art = 'GrassHopper'
-img = np.array(plt.imread(f'./images/{art}.png')[..., :3] * 0xff)
+img = array(plt.imread(f'./images/{art}.png')[..., :3] * 0xff)
 
+#   'A' stands for signal amplification (used for presentation purposes only: to help'em shine!)
+A = lambda x, a = 0x10, l = -0xff, h = 0xff: clip(a*x, l, h)
 ##  'λ' is - as always - a λevǝλ of poison (a.k.a an intensity in Poisson distribution)
-wn, λ = eval(sys.argv[1]) if len(sys.argv) > 1 else 'bior1.1', 0 # 'Haar" will do too!
-if λ > 0: img = poisson(img * 2**λ)/2**λ
-
-#   'A' stands for amplification of a signal and is used for presentation purposes only
-#   (i.e. to help'em shine!)
-A, wn = lambda x, a = 0x10, l = -0xff, h = 0xff: np.clip(a*x, l, h), 'Haar'
+wn, λ = eval(sys.argv[1]) if len(sys.argv) > 1 else ('bior1.1', 0) # 'Haar" will do too!
 
 # Ready, set, go!
 tabs, titles  = mtw(), [f'{wn} approximation (LL)', 'Horizontal details (HL)',
-                        'Vertical details (LH)',   'Diagonal details (HH)']
+                         'Vertical details (LH)',   'Diagonal details (HH)']
 # Open it...
-img = np.array(plt.imread(f'./images/{art}.png')[..., :3] * 0xff)
+img = array(plt.imread(f'./images/{art}.png')[..., :3] * 0xff)
+# ... add some poison...
+if λ != 0: img = poisson(img * 2**λ)/2**λ
 # ... do the color transform...
 img = img@RGB2YCxCy.T
-# ... and pick th eluminance channel only
+# ... and pick the luminance channel only
 Y = img[..., 0]
-##  MRA quartet decomposition (analysis - a forward wavelet transform):
-#   one approximation (LL) and three details/differences quarters (HL, LH, HH)
-#   You can call it a "wavelet microscope".
+
+##  MRA quartet decomposition (a forward wavelet transform - analysis): one approximation (LL) 
+#   and three details/differences quarters (HL, LH, HH). You can call it a "wavelet microscope".
 LL, (HL, LH, HH) = dwt2(Y, wn)
 di([A(LL, .5), A(HL), A(LH), A(HH)], titles, title = 'Haar MRA decomposition', grid = False, tabs = tabs)
 
 ##  They ♪♫ask no quarters♫♪ https://youtu.be/_vG_mTt6hCs,
-#   so let's "integrate" (as there is no single way to skin the cat, is it?)
-#   them back to let them shine in all their glory
-MRA = np.concatenate([np.concatenate([A(LL, .5), A(HL)], axis = 1),
-                      np.concatenate([A(LH), A(HH)], axis = 1)], axis = 0)
-
-#   Reconstruction from MRA (synthesis - an inverse wavelet transform)
+#   so let's "integrate" (there is no single way to skin the cat - or is it?) them back
+#   to help'em shine in all their glory
+MRA = cnc([cnc([A(LL, .5), A(HL)], axis = 1),
+           cnc([A(LH),     A(HH)], axis = 1)], axis = 0)
+#   Reconstruction from MRA (an inverse wavelet transform - synthesis)
 Y = idwt2((LL, (HL, LH, HH)), wn)
 #   It looks like eventually "nothing [zero, zip, zilch, nada] happened here, move along people"!
 di([Y, MRA, Y - img[..., 0]], ['DWT⁻¹(DWT(Y)) → ⌊…⌋ ?', 'MRA', 'Zero, zip, zilch, nada?'],
-   title = 'The MRA, or There and Back Again...', grid = False, tabs = tabs)
+    title = 'The MRA, or There and Back Again...', grid = False, tabs = tabs)
 
 # "That's all, folks!"
 tabs.show()
