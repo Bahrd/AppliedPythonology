7bcf4db8d609311f1f79ef2130539d24b3a00f0b
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2025-01-23 22:23:27 +0100

    Redundancy reduction...

--- WaveletMRA.py
+++ WaveletMRA.py
@@ -2,7 +2,7 @@
 #   See https://pywavelets.readthedocs.io/en/latest/
 #   Illustrated here for the luminance ('Y', grayscale) channel
 #   from the YCoCg space and (be default) with the help of the "grandpas" of all wavelets:
-#   the "International sensation!" Haar family! [ https://youtu.be/uMTPXen99n0 ]
+#   the "International sensation!", the Haar family! [ https://youtu.be/uMTPXen99n0 ]
 #   Some say: "You can choose your friends, but you can't choose your family!"
 #   Oh, really?! :D
 
@@ -16,10 +16,6 @@ from pywt import dwt2, idwt2
 
 ##  The "GrassHopper" is a "friend of horses" (Philip) and a "spirit" (Mustang)...
 art = 'GrassHopper'
-img = array(plt.imread(f'./images/{art}.png')[..., :3] * 0xff)
-
-#   'A' stands for signal amplification (used for presentation purposes only: to help'em shine!)
-A = lambda x, a = 0x10, l = -0xff, h = 0xff: clip(a*x, l, h)
 ##  'λ' is - as always - a λevǝλ of poison (a.k.a an intensity in Poisson distribution)
 wn, λ = eval(sys.argv[1]) if len(sys.argv) > 1 else ('bior1.1', 0) # 'Haar" will do too!
 
@@ -35,18 +31,27 @@ img = img@RGB2YCxCy.T
 # ... and pick the luminance channel only
 Y = img[..., 0]
 
-##  MRA quartet decomposition (a forward wavelet transform - analysis): one approximation (LL) 
-#   and three details/differences quarters (HL, LH, HH). You can call it a "wavelet microscope".
+##  MRA quartet decomposition|analysis (a forward wavelet transform): 
+#   + one approximation (LL), and 
+#   + three details/differences quarters (HL, LH, HH). 
+#   You can call it a "wavelet microscope"
 LL, (HL, LH, HH) = dwt2(Y, wn)
-di([A(LL, .5), A(HL), A(LH), A(HH)], titles, title = 'Haar MRA decomposition', grid = False, tabs = tabs)
-
-##  They ♪♫ask no quarters♫♪ https://youtu.be/_vG_mTt6hCs,
-#   so let's "integrate" (there is no single way to skin the cat - or is it?) them back
-#   to help'em shine in all their glory
-MRA = cnc([cnc([A(LL, .5), A(HL)], axis = 1),
-           cnc([A(LH),     A(HH)], axis = 1)], axis = 0)
-#   Reconstruction from MRA (an inverse wavelet transform - synthesis)
+#   ...
+#   Note that we do nothing like 
+#   + thresholding or
+#   + quantizing 
+#   the wavelet coefficients...
+#   ...
+#   Reconstruction|synthesis from MRA (an inverse wavelet transform)
 Y = idwt2((LL, (HL, LH, HH)), wn)
+
+##  MRA demonstration
+A = 0x10; LL, (HL, LH, HH) = LL/2, (HL*A, LH*A, HH*A)
+di([LL, HL, LH, HH], titles, title = 'Haar MRA decomposition', grid = False, tabs = tabs)
+##  They ♪♫ask no quarters♫♪ https://youtu.be/_vG_mTt6hCs, so let's "integrate" 
+#   (there is no single way to skin the cat - or is it?) them back to help'em shine in all their glory
+MRA = cnc([cnc([LL, HL], axis = 1), 
+           cnc([LH, HH], axis = 1)], axis = 0)
 #   It looks like eventually "nothing [zero, zip, zilch, nada] happened here, move along people"!
 di([Y, MRA, Y - img[..., 0]], ['DWT⁻¹(DWT(Y)) → ⌊…⌋ ?', 'MRA', 'Zero, zip, zilch, nada?'],
     title = 'The MRA, or There and Back Again...', grid = False, tabs = tabs)
